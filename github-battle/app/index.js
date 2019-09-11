6d0e227b98624d4c61ffb54ba2b7379fdba451e2
Paul Tobin <tobster111@gmail.com>
2019-09-11 09:07:03 +0100

    Showing conditional rendering in JSX with javascript

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-class App extends Component {
+function isAuthed() {
+  return true;
+}
+
+function isNew() {
+  return false;
+}
+
+export default class App extends Component {
   render() {
-    return <div>Hello Paul!</div>;
+    const authed = isAuthed();
+    const firstLogin = isNew();
+
+    if (firstLogin === true) {
+      return <h1>Welcome!</h1>;
+    } else if (authed === true) {
+      return <h1>Welcome back!</h1>;
+    } else {
+      return <h1>Login to see your dashboard</h1>;
+    }
   }
 }
 
 ReactDom.render(<App />, document.getElementById('app'));
-
