b61a47cc04bcc94b54288efb5ca49dfafeaefdeb
Paul Tobin <tobster111@gmail.com>
2019-09-12 10:27:29 +0100

    Added a button in each unordered list

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react';
 export default class componentName extends Component {
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-    return <ul className="flex-center">
-      {languages.map((language, index) => {
-        return <li key={index}>{language}</li>
-      })}
-    </ul>;
+    return (
+      <ul className="flex-center">
+        {languages.map(language => (
+          <li key={language}>
+            <button className="btn-clear nav-link">{language}</button>
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
