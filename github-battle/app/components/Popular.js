2d071a4666bac7fc73b68f0819ceb324e0460e9c
Paul Tobin <tobster111@gmail.com>
2019-09-16 08:59:56 +0100

    Added state for repos and errors, used fetchPopularRepos from utils to make the api calls

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Proptypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -23,19 +24,33 @@ function LanguagesNav({ selected, onUpdateLanguage }) {
 LanguagesNav.prototype = {
   selected: Proptypes.string.isRequired,
   onUpdateLanguage: Proptypes.func.isRequired
-}
+};
 
 export default class Popular extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null
     };
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
   updateLanguage(selectedLanguage) {
-    this.setState({ selectedLanguage });
+    this.setState({ selectedLanguage, error: null, repos: null });
+
+    fetchPopularRepos(selectedLanguage).then(repos =>
+      this.setState({
+        repos,
+        error: null
+      })
+    ).catch(() => {
+      console.warn('Error fetching repos: ', error)
+      this.setState({
+        error: `There was an error fetching the repositories`
+      })
+    })
   }
 
   render() {
