8610594f92bd582486952f428882bdcb9654a37c
Paul Tobin <tobster111@gmail.com>
2019-09-17 10:06:04 +0100

    Destructing the need data from the api call

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -28,8 +28,34 @@ LanguagesNav.prototype = {
 
 function ReposGrid({ repos }) {
   return (
-    <ul>
-      <pre>{JSON.stringify(repos, null, 2)}</pre>
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const {
+          name,
+          owner,
+          html_url,
+          statgazers_count,
+          forks,
+          open_issues
+        } = repo;
+        const { login, avatar_url } = owner;
+
+        return (
+          <li key={html_url} className="repo bg-light">
+            <h4 className="header-lg center-text">#(index + 1)</h4>
+            <img
+              className="avatar"
+              src={avatar_url}
+              alt={`Avatar for ${login}`}
+            />
+            <h2 className="center-text">
+              <a className="link" href={html_url}>
+                {login}
+              </a>
+            </h2>
+          </li>
+        );
+      })}
     </ul>
   );
 }
