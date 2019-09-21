1b6b8ba4223dfb32a453183e485c1f5dde49d519
Paul Tobin <tobster111@gmail.com>
2019-09-21 10:14:05 +0100

    Added a constructor to Battle.js allow two player name to be submitted

--- github-battle/app/components/Battle.js
+++ github-battle/app/components/Battle.js
@@ -86,12 +86,48 @@ PlayerInput.propsTypes = {
 };
 
 export default class componentName extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    })
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state;
     return (
-      <div>
+      <React.Fragment>
         <Instructions />
+        <div class="players-container">
+          <h1 className="center-text header-lg">Players</h1>
+          <div className="row space-around">
+            {playerOne === null && (
+              <PlayerInput
+                label="Player One"
+                onSubmit={player => this.handleSubmit('playerOne', player)}
+              />
+            )}
+
+            {playerTwo === null && (
+              <PlayerInput
+                label="Player Two"
+                onSubmit={player => this.handleSubmit('playerTwo', player)}
+              />
+            )}
+          </div>
+        </div>
         <PlayerInput label="label!" onSubmit={value => console.log(value)} />
-      </div>
+      </React.Fragment>
     );
   }
 }
