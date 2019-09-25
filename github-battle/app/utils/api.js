fa5c313027cb6817227e432c491bc57c2eeef25c
Paul Tobin <tobster111@gmail.com>
2019-09-25 11:19:17 +0100

    Added getErrorMsg to api.js

--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -2,6 +2,14 @@ const id = 'YOUR_CLIENT_ID';
 const sec = 'YOUR_SECRET_ID';
 const params = `?client_id=${id}&client_secret=${sec}`;
 
+function getErrorMsg(message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`;
+  }
+
+  return message;
+}
+
 function getProfile(username) {
   return fetch(`https://api.github.com/users/${username}${params}`)
     .then(res => res.json)
