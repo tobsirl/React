57ba354cba085c9d95073edc5e3cc56c692bbbf6
Paul Tobin <tobster111@gmail.com>
2019-09-28 11:30:24 +0100

    Added Card.js a class based component

---
+++ github-battle/app/components/Card.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Card</h1>
+      </div>
+    );
+  }
+}
