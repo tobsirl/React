d8f025025ab5aa32db5933425e5821814193dbe2
Paul Tobin <tobster111@gmail.com>
2019-09-28 12:23:59 +0100

    Refactored for the loser side of the results component

--- github-battle/app/components/Card.js
+++ github-battle/app/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Card({ header, subheader, avatar, href, name }) {
+export default function Card({ header, subheader, avatar, href, name, children }) {
   return (
     <div className="card bg-light">
       <h4 className="header-lg center-text">{header}</h4>
@@ -12,6 +12,7 @@ export default function Card({ header, subheader, avatar, href, name }) {
           {name}
         </a>
       </h2>
+      {children}
     </div>
   );
 }
--- github-battle/app/components/Results.js
+++ github-battle/app/components/Results.js
@@ -89,22 +89,13 @@ export default class Results extends Component {
             </li>
           </ul>
         </Card>
-
-        <div className="card bg-light">
-          <h4 className="header-lg center-text">
-            {winner.score === loser.score ? 'Tie' : 'Loser'}
-          </h4>
-          <img
-            className="avatar"
-            src={loser.profile.avatar_url}
-            alt={`Avatar for ${loser.profile.login}`}
-          />
-          <h4 className="center-text">Score: {loser.score.toLocaleString()}</h4>
-          <h2 className="center-text">
-            <a className="link" href={loser.profile.html_url}>
-              {loser.profile.login}
-            </a>
-          </h2>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          href={loser.profile.html_url}
+          name={loser.profile.login}
+        >
           <ul className="card-list">
             <li>
               <FaUser color="rgb(239, 115, 115)" size={22} />
@@ -131,7 +122,7 @@ export default class Results extends Component {
               {loser.profile.following.toLocaleString()} following
             </li>
           </ul>
-        </div>
+        </Card>
       </div>
     );
   }
