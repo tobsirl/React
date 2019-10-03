4639f54a7442656100dc24099806cce40ada190c
Paul Tobin <tobster111@gmail.com>
2019-10-03 09:45:40 +0100

    Adding styling to the loading component, adding the styling to the paragraph

--- github-battle/app/components/Loading.js
+++ github-battle/app/components/Loading.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 
+const styles = {
+  content: {
+    fontSize: '35px',
+    position: 'absolute',
+    left: '0',
+    right: '0',
+    marginTop: '20px',
+    textAlign: 'center'
+  }
+};
+
 export default class Loading extends Component {
   constructor(props) {
     super(props);
@@ -24,6 +35,6 @@ export default class Loading extends Component {
   }
 
   render() {
-    return <p>{this.state.content}</p>;
+    return <p style={styles.content}>{this.state.content}</p>;
   }
 }
