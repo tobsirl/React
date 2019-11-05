9e42f8720104c3cd77b47d647bb252964fdae211
Paul Tobin <tobster111@gmail.com>
2019-11-05 09:00:38 +0000

    Creating a tooltip component that extracts the functionality of the tooltip, styles, methods and the constructor

--- github-battle/app/components/Results.js
+++ github-battle/app/components/Results.js
@@ -12,51 +12,8 @@ import Card from './Card';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
 
-const styles = {
-  container: {
-    position: 'relative',
-    display: 'flex'
-  },
-  tooltip: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    width: '160px',
-    bottom: '100%',
-    left: '50%',
-    marginLeft: '-80px',
-    borderRadius: '3px',
-    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-    padding: '7px',
-    marginBottom: '5px',
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: '14px'
-  }
-};
-
 class ProfileList extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      hoveringLocation: false,
-      hoveringCompany: false
-    };
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
-
-  mouseOver(id) {
-    this.setState({
-      [id]: true
-    });
-  }
-
-  mouseOut(id) {
-    this.setState({
-      [id]: false
-    });
-  }
 
   render() {
     const { profile } = this.props;
