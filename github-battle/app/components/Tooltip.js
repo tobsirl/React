8f6a47c91af2f853cb8c7efe94ac27b244656b85
Paul Tobin <tobster111@gmail.com>
2019-11-06 12:00:56 +0000

    Create the withHover HOC that take a component and returns a component, withHover has the constructor and methods in its body

--- github-battle/app/components/Tooltip.js
+++ github-battle/app/components/Tooltip.js
@@ -25,27 +25,7 @@ const styles = {
 };
 
 // export default class Tooltip extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       hovering: false
-//     };
-//     this.mouseOver = this.mouseOver.bind(this);
-//     this.mouseOut = this.mouseOut.bind(this);
-//   }
-
-//   mouseOver() {
-//     this.setState({
-//       hovering: true
-//     });
-//   }
-
-//   mouseOut() {
-//     this.setState({
-//       hovering: false
-//     });
-//   }
+//   
 
 //   render() {
 //     const { text, children } = this.props;
