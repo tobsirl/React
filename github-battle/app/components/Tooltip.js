b6920de9b9709037c5908ef267dd2ed6bb73696d
Paul Tobin <tobster111@gmail.com>
2019-11-07 15:08:38 +0000

    Changing from HOC to using render props

--- github-battle/app/components/Tooltip.js
+++ github-battle/app/components/Tooltip.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import withHover from './withHover';
+import Hover from './Hover';
 
 const styles = {
   container: {
@@ -38,4 +38,4 @@ Tooltip.propTypes = {
   hovering: PropTypes.bool.isRequired
 };
 
-export default withHover(Tooltip, 'hover');
+export default Tooltip;
