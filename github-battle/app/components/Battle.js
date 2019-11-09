89f17ec85dd08b208e79917f5f2e53e0c2181987
Paul Tobin <tobster111@gmail.com>
2019-11-09 09:26:42 +0000

    Using the ThemeConsumer in PlayerPreview, same pattern, render props

--- github-battle/app/components/Battle.js
+++ github-battle/app/components/Battle.js
@@ -110,24 +110,28 @@ PlayerInput.propTypes = {
 
 function PlayerPreview({ username, onReset, label }) {
   return (
-    <div className="column player">
-      <h3 className="player-label">{label}</h3>
-      <div className="row bg-light">
-        <div className="player-info">
-          <img
-            className="avatar-small"
-            src={`https://github.com/${username}.png?size=200`}
-            alt={`Avatar for ${username}`}
-          />
-          <a href={`https://github.com/${username}`} className="link">
-            {username}
-          </a>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className="column player">
+          <h3 className="player-label">{label}</h3>
+          <div className="row bg-light">
+            <div className="player-info">
+              <img
+                className="avatar-small"
+                src={`https://github.com/${username}.png?size=200`}
+                alt={`Avatar for ${username}`}
+              />
+              <a href={`https://github.com/${username}`} className="link">
+                {username}
+              </a>
+            </div>
+            <button className="btn-clear flex-center" onClick={onReset}>
+              <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+            </button>
+          </div>
         </div>
-        <button className="btn-clear flex-center" onClick={onReset}>
-          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
-        </button>
-      </div>
-    </div>
+      )}
+    </ThemeConsumer>
   );
 }
 
