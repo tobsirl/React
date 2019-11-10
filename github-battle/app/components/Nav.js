e7d715cc151cff53096e0db101edd13de964cb4f
Paul Tobin <tobster111@gmail.com>
2019-11-10 11:01:50 +0000

    React-Router - Using activeStyles on both links, using exact on '/'

--- github-battle/app/components/Nav.js
+++ github-battle/app/components/Nav.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { ThemeConsumer } from '../contexts/theme';
 import { NavLink } from 'react-router-dom';
 
+const activeStyle = {
+  color: 'rgb(187, 46, 31)'
+};
+
 export default function Nav() {
   return (
     <ThemeConsumer>
@@ -9,12 +13,21 @@ export default function Nav() {
         <nav className="row space-between">
           <ul className="row nav">
             <li>
-              <NavLink to="/" className="nav-link">
+              <NavLink
+                to="/"
+                exact
+                activeStyle={activeStyle}
+                className="nav-link"
+              >
                 Popular
               </NavLink>
             </li>
             <li>
-              <NavLink to="/battle" className="nav-link">
+              <NavLink
+                to="/battle"
+                activeStyle={activeStyle}
+                className="nav-link"
+              >
                 Battle
               </NavLink>
             </li>
