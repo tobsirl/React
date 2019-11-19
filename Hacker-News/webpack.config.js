925e05fd1088ca8c2a88eb489b150cfbf55da6e7
Paul Tobin <tobster111@gmail.com>
2019-11-19 15:31:46 +0000

    Adding babel presets to package.json

--- Hacker-News/webpack.config.js
+++ Hacker-News/webpack.config.js
@@ -5,5 +5,12 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  mode: 'development'
 };
