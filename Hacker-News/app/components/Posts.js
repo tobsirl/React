c69439e4147b20df203b5e842faf1b275a79035e
Paul Tobin <tobster111@gmail.com>
2019-11-21 17:27:24 +0000

    Added a constructor to the posts component + handleFetch() method that is called in componentDidMount

--- Hacker-News/app/components/Posts.js
+++ Hacker-News/app/components/Posts.js
@@ -2,20 +2,41 @@ import React, { Component } from 'react';
 import { fetchMainPosts } from '../utils/api';
 
 export default class Posts extends Component {
-  state = {
-    posts: null,
-    error: null,
-    loading: true
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: null,
+      error: null,
+      loading: true
+    };
+  }
 
   componentDidMount() {
     this.handleFetch();
   }
 
+  handleFetch() {
+    this.setState({
+      posts: null,
+      error: null,
+      loading: true
+    });
+
+    fetchMainPosts(this.props.type).then(posts =>
+      this.setState({
+        posts,
+        loading: false,
+        error: null
+      })
+    );
+  }
+
   render() {
+    console.log(this.state.posts);
     return (
       <div className="container">
         <h1>Hacker</h1>
+        {JSON.stringify(this.state.posts)}
       </div>
     );
   }
