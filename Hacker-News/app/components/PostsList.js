6354a589c1a813eb6ac064d515a46eae3d89b93c
Paul Tobin <tobster111@gmail.com>
2019-11-22 20:28:32 +0000

    Formatting the output for post by and date

--- Hacker-News/app/components/PostsList.js
+++ Hacker-News/app/components/PostsList.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 export default function PostsList(props) {
-  console.log(props.posts)
+  console.log(props.posts);
   return (
     <div>
-      <ul>{props.posts.map((post) => {
-        return <li key={post.id}>{post.title}</li>
-      })}</ul>
+      <ul>
+        {props.posts.map(post => {
+          return (
+            <div>
+              <li key={post.id}>{post.title}</li>
+              <p>
+                by {post.by} on {Date(post.time)}
+              </p>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 }
