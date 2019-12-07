8ed592c7e039bb5ebf4e1fbd0b7d9098d9115dc8
Paul Tobin <tobster111@gmail.com>
2019-12-07 20:55:12 +0000

    Forms practice
    Controlled, using a single onChange with ES6 computed property names

--- starwars/src/components/Forms.js
+++ starwars/src/components/Forms.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
 
-export default class componentName extends Component {
+export default class Forms extends Component {
+  state = {
+    firstName: '',
+    lastName: ''
+  };
+
+  handleChange = event => {
+    console.log(event.target);
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <Fragment>
+        <div>
+          <label htmlFor="firstname">
+            <input
+              type="text"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+              name="firstName"
+              placeholder="First Name"
+            />
+          </label>
+          <br />
+          <label htmlFor="lastname">
+            <input
+              type="text"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+              name="lastName"
+              placeholder="Last Name"
+            />
+          </label>
+          <span>{this.state.firstName}</span>
+          <br />
+          <span>{this.state.lastName}</span>
+        </div>
+      </Fragment>
+    );
   }
 }
