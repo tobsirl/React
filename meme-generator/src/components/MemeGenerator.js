fdd1b60b7abe54598737531128fc712ae516d622
Paul Tobin <tobster111@gmail.com>
2019-12-09 17:18:50 +0000

    Using componentDidMount to make the api request

--- meme-generator/src/components/MemeGenerator.js
+++ meme-generator/src/components/MemeGenerator.js
@@ -4,9 +4,19 @@ class MemeGenerator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      topText: '',
+      bottomText: '',
+      randomImage: 'http://i.imgflip.com/1bij.jpg',
+      allMemeImgs: []
     };
   }
+
+  componentDidMount() {
+    fetch('https://api.imgflip.com/get_memes').then(res =>
+      res.json().then(data => this.setState({ allMemeImgs: data.data }))
+    );
+  }
+
   render() {
     return (
       <div>
