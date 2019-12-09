2d41d5a7dba6e459ca3be475f92867209b8c03c9
Paul Tobin <tobster111@gmail.com>
2019-12-09 16:49:45 +0000

    Created two components, one functional Header and one class based MemeGenerator

---
+++ meme-generator/src/components/MemeGenerator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MemeGenerator extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Meme Generator</h1>
+      </div>
+    );
+  }
+}
+
+export default MemeGenerator;
