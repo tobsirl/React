8ce2a1d2bd0ba59f2044a8fae11eb889ef6c06c6
Paul Tobin <tobster111@gmail.com>
2019-12-09 17:40:31 +0000

    Setup top text and bottom text forms with handleChange

--- meme-generator/src/components/MemeGenerator.js
+++ meme-generator/src/components/MemeGenerator.js
@@ -9,18 +9,46 @@ class MemeGenerator extends Component {
       randomImage: 'http://i.imgflip.com/1bij.jpg',
       allMemeImgs: []
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch('https://api.imgflip.com/get_memes').then(res =>
-      res.json().then(data => this.setState({ allMemeImgs: data.data }))
+      res.json().then(memes => this.setState({ allMemeImgs: memes.data }))
     );
   }
 
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
+    // console.log(this.state.allMemeImgs);
     return (
       <div>
-        <h1>Meme Generator</h1>
+        <form className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            value={this.state.topText}
+            onChange={this.handleChange}
+            placeholder="Top Text"
+          />
+          <input
+            type="text"
+            name="bottomText"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+            placeholder="Bottom Text"
+          />
+
+          <button>Gen</button>
+        </form>
       </div>
     );
   }
