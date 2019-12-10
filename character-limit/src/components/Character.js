97a2b65a1c8d162235da56882d2da5805598f0af
Paul Tobin <tobster111@gmail.com>
2019-12-10 17:38:23 +0000

    Example of using useState and useEffect to count the characters in a textarea

--- character-limit/src/components/Character.js
+++ character-limit/src/components/Character.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Character() {
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    document.title = `${240 - input.length} characters`;
+  }, [input]);
+
   return (
     <div>
-      <h1>Hi</h1>
+      <textarea
+        type="text"
+        value={input}
+        placeholder="Type text here!"
+        onChange={e => setInput(e.target.value)}
+      />
+      <button disabled={input.length === 0 || input.length > 240}>
+        Sumbit
+      </button>
+      <br />
+      <h1>{input}</h1>
     </div>
-  )
+  );
 }
 
 export default Character;
