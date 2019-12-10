60f2a97d4c442e0c4d0551a9d1133c6e3832b2b9
Paul Tobin <tobster111@gmail.com>
2019-12-10 17:26:12 +0000

    Setup Character project for using useState and useEffect

---
+++ character-limit/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Character from './components/Character';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Character />
+    </div>
+  );
+}
+
+export default App;
---
+++ character-limit/src/components/Character.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Character() {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default Character;
