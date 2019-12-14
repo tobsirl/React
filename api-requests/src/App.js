c32d9c42d0e1be040912720922aeae7096a18cef
Paul Tobin <tobster111@gmail.com>
2019-12-14 11:35:52 +0000

    Added state
    Added state for index, loading, error and post

--- api-requests/src/App.js
+++ api-requests/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const postIds = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function fetchPost(id) {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res =>
+    res.json()
+  );
+}
+
 function App() {
+  const [index, setIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [post, setPost] = useState(null);
+
   return (
     <div className="App">
       <h1>App</h1>
