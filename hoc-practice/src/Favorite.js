fdca47ac4ad626bb7c3bca9a1042c3be0d3fb8ec
Paul Tobin <tobster111@gmail.com>
2019-12-16 17:43:49 +0000

    Favourite is working with props received from HOC

--- hoc-practice/src/Favorite.js
+++ hoc-practice/src/Favorite.js
@@ -2,26 +2,12 @@ import React, { Component } from 'react';
 import { withToggle } from './HOC/withToggle';
 
 class Favorite extends Component {
-  state = {
-    isFavorited: false
-  };
-
-  toggleFavorite = () => {
-    this.setState(prevState => {
-      return {
-        isFavorited: !prevState.isFavorited
-      };
-    });
-  };
-
   render() {
     return (
       <div>
         <h3>Click heart to favourite</h3>
         <h1>
-          <span onClick={this.toggleFavorite}>
-            {this.state.isFavorited ? '💗' : '💓'}
-          </span>
+          <span onClick={this.props.toggle}>{this.props.on ? '💗' : '💓'}</span>
         </h1>
       </div>
     );
