488352d0aed57a6f280375eb25400163b131f57e
Paul Tobin <tobster111@gmail.com>
2019-12-16 17:19:12 +0000

    Finished the Favourite

--- hoc-practice/src/App.js
+++ hoc-practice/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Favorite from './Favorite'
 
 import './App.css';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Favorite />
+    </div>
+  )
+  
 }
 
 export default App;
---
+++ hoc-practice/src/Favorite.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Favorite extends Component {
+  state = {
+    isFavorited: false
+  };
+
+  toggleFavorite = () => {
+    this.setState(prevState => {
+      return {
+        isFavorited: !prevState.isFavorited
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Click heart to favourite</h3>
+        <h1>
+          <span onClick={this.toggleFavorite}>
+            {this.state.isFavorited ? '💗' : '💓'}
+          </span>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Favorite;
