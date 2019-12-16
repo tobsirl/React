cd1e6c8eaeefd8de4afc3954bfe990fe81663b0b
Paul Tobin <tobster111@gmail.com>
2019-12-16 17:42:28 +0000

    Menu is working with props received from HOC

--- hoc-practice/src/Menu.js
+++ hoc-practice/src/Menu.js
@@ -3,25 +3,13 @@ import React, { Component } from 'react';
 import { withToggle } from './HOC/withToggle';
 
 class Menu extends Component {
-  state = {
-    show: true
-  };
-
-  toggleShow = () => {
-    this.setState(prevState => {
-      return {
-        show: !prevState.show
-      };
-    });
-  };
-
   render() {
     return (
       <div>
-        <button onClick={this.toggleShow}>
-          {this.state.show ? 'Hide' : 'Show'} Menu{' '}
+        <button onClick={this.props.toggle}>
+          {this.props.on ? 'Hide' : 'Show'} Menu{' '}
         </button>
-        <nav style={{ display: this.state.show ? 'block' : 'none' }}>
+        <nav style={{ display: this.props.on ? 'block' : 'none' }}>
           <h6>Signed in as Coder123</h6>
           <a>Your Profile</a>
           <a>Your Repositories</a>
