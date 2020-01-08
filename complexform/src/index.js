ade7db402c5353cf022b27bab2d0e942ca4141ce
Paul Tobin <tobster111@gmail.com>
2020-01-08 16:37:19 +0000

    Setup of complex forms practice

--- complexform/src/index.js
+++ complexform/src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+  Instructions:
+    You're given the UI for a basic form. Your task is to 
+    hook it all up using refs. 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    The `Focus X Input` buttons should focus that specific input
+    field.
+
+    The `Submit` button should log `name`, `email`, and `password`
+    to the console.
+
+    The `Reset` button should result all of the input fields to 
+    empty strings.
+*/
+
+function Form() {
+  const handleSubmit = e => {};
+
+  const handleReset = () => {};
+
+  return (
+    <React.Fragment>
+      <label>
+        Name:
+        <input placeholder="name" type="text" />
+      </label>
+      <label>
+        Email:
+        <input placeholder="email" type="text" />
+      </label>
+      <label>
+        Password:
+        <input placeholder="password" type="text" />
+      </label>
+
+      <hr />
+
+      <button>Focus Name Input</button>
+      <button>Focus Email Input</button>
+      <button>Focus Password Input</button>
+
+      <hr />
+
+      <button>Submit</button>
+      <button>Reset</button>
+    </React.Fragment>
+  );
+}
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Form />, rootElement);
