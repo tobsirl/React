0a3febbf5382a3b93533fb83dd199df738981771
Paul Tobin <tobster111@gmail.com>
2020-01-16 19:14:52 +0000

    github-battle-hooks - Added theme and setTheme using useState with a default value of 'light'

--- github-battle-hooks/app/index.js
+++ github-battle-hooks/app/index.js
@@ -1,46 +1,40 @@
-import React, { Component } from "react";
-import ReactDom from "react-dom";
-import "./index.css";
-import { ThemeProvider } from "./contexts/theme";
-import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Loading from "./components/Loading";
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import { ThemeProvider } from './contexts/theme';
+import Nav from './components/Nav';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loading from './components/Loading';
 
-const Popular = React.lazy(() => import("./components/Popular"));
-const Battle = React.lazy(() => import("./components/Battle"));
-const Results = React.lazy(() => import("./components/Results"));
+const Popular = React.lazy(() => import('./components/Popular'));
+const Battle = React.lazy(() => import('./components/Battle'));
+const Results = React.lazy(() => import('./components/Results'));
 
-export default class App extends Component {
-  state = {
-    theme: "light",
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === "light" ? "dark" : "light"
-      }));
-    }
-  };
+export default function App() {
+  const [theme, setTheme] = useState('light');
 
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className="container">
-              <Nav />
-              <React.Suspense fallback={Loading}>
-                <Switch>
-                  <Route exact path="/" component={Popular} />
-                  <Route exact path="/battle" component={Battle} />
-                  <Route path="/battle/results" component={Results} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
+  const toggleTheme = () =>
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'));
+
+  return (
+    <Router>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className="container">
+            <Nav toggleTheme={toggleTheme} />
+            <React.Suspense fallback={Loading}>
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
           </div>
-        </ThemeProvider>
-      </Router>
-    );
-  }
+        </div>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(<App />, document.getElementById('app'));
