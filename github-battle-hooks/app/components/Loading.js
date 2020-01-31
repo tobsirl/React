9389287ff571aa911d82607cb32a33b46f422571
Paul Tobin <tobster111@gmail.com>
2020-01-31 18:31:23 +0000

    useEffect calling setInterval to change the content using setContent

--- github-battle-hooks/app/components/Loading.js
+++ github-battle-hooks/app/components/Loading.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const styles = {
@@ -12,43 +12,25 @@ const styles = {
   }
 };
 
-export default function Loading({text = 'Loading', speed = 300}) {
+export default function Loading({ text = 'Loading', speed = 300 }) {
   const [content, setContent] = useState(text);
 
-  
-    return <p style={styles.content}>{content}</p>;
-  
-
-}
-
-
-export default class Loading extends Component {
-  state = {
-    content: this.props.text
-  };
-
-  componentDidMount() {
-    const { speed, text } = this.props;
-
-    this.interval = window.setInterval(() => {
-      this.state.content === text + '...'
-        ? this.setState({ content: text })
-        : this.setState(({ content }) => ({ content: content + '.' }));
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      setContent(content => {
+        return content === `${text}...` ? text : `${content}.`;
+      });
     }, speed);
-  }
 
-  componentWillUnmount() {
-    window.clearInterval(this.interval);
-  }
+    return () => {
+      window.clearInterval(id);
+    };
+  }, [text, speed]);
 
-  render() {
-    return <p style={styles.content}>{this.state.content}</p>;
-  }
+  return <p style={styles.content}>{content}</p>;
 }
 
 Loading.propTypes = {
   text: PropTypes.string.isRequired,
   speed: PropTypes.number.isRequired
 };
-
-
