0c0cf1bdcb2f4dc884668f4c2ba2ce89d0dd0dab
Paul Tobin <tobster111@gmail.com>
2020-03-18 11:31:50 +0000

    Adding Button.js and Header.js

---
+++ context-api/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Button() {
+  return <button className="dark-theme">Switch Theme</button>;
+}
+
+export default Button;
---
+++ context-api/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="dark-theme">
+      <h2>Theme</h2>
+    </header>
+  );
+}
+
+export default Header;
