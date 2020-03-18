423bc4527f101f59844be4352f1f7eaf8a7f53fa
Paul Tobin <tobster111@gmail.com>
2020-03-18 11:36:19 +0000

    Create themeContext.js, import and wrap <App /> in ThemeContext.Provider

--- context-api/src/index.js
+++ context-api/src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ThemeContext from './themeContext';
+
+ReactDOM.render(
+  <ThemeContext>
+    <App />
+  </ThemeContext>,
+  document.getElementById('root')
+);
