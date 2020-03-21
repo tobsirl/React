0426bd287a00a5bad1dea17c4834332836a9e61e
Paul Tobin <tobster111@gmail.com>
2020-03-21 12:16:34 +0000

    Using controlled form for the textarea

--- speedtyping/src/App.js
+++ speedtyping/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const handleInputChange = e => {
+    setInput({
+      ...input,
+      [e.currentTarget.name]: e.currentTarget.value
+    });
+  };
+
   return (
     <div>
       <h1>How fast can you type?</h1>
-      <textarea name="" id="" cols="20" rows="10" />
+      <textarea name="input" onChange={handleInputChange} />
       <h4>Time Remaining:</h4>
       <button>Start</button>
       <h1>Word Count:</h1>
