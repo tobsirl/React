65a71a380b04f95a291223222c2230e66c4e8252
Paul Tobin <tobster111@gmail.com>
2020-03-21 11:58:50 +0000

    Setup basic project with markup and imported Press Start 2P font

---
+++ speedtyping/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>How fast can you type?</h1>
+      <textarea name="" id="" cols="20" rows="10" />
+      <h4>Time Remaining:</h4>
+      <button>Start</button>
+      <h1>Word Count:</h1>
+    </div>
+  );
+}
+
+export default App;
