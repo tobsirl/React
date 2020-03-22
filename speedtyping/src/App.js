338e5c388e407b895ac067b8f1d4de7be6da78ae
Paul Tobin <tobster111@gmail.com>
2020-03-22 11:12:27 +0000

    useEffect to countdown the time remaining

--- speedtyping/src/App.js
+++ speedtyping/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 function App() {
   const [text, setText] = useState('');
   const [wordCount, setWordCount] = useState(0);
+  const [countDown, setCountDown] = useState(15);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -18,11 +19,21 @@ function App() {
     setWordCount(count);
   }
 
+  useEffect(() => {
+    const id = setTimeout(() => {
+      if (countDown === 0) return;
+      setCountDown(countDown - 1);
+    }, 1000);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [countDown]);
+
   return (
     <div>
       <h1>How fast can you type?</h1>
       <textarea value={text} onChange={handleChange} />
-      <h4>Time Remaining:</h4>
+      <h4>Time Remaining: {countDown}</h4>
       <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word Count: {wordCount}</h1>
     </div>
