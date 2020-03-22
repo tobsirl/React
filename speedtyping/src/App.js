1f57dccee3e62fc48e8e1d020af7e6e8ff8615f9
Paul Tobin <tobster111@gmail.com>
2020-03-22 10:53:01 +0000

    Implemented calculateWordCount function with a useState wordCount

--- speedtyping/src/App.js
+++ speedtyping/src/App.js
@@ -3,22 +3,28 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [input, setInput] = useState('');
+  const [text, setText] = useState('');
+  const [wordCount, setWordCount] = useState(0);
 
-  const handleInputChange = e => {
-    setInput({
-      ...input,
-      [e.currentTarget.name]: e.currentTarget.value
-    });
-  };
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  function calculateWordCount(text) {
+    console.log(text);
+    const wordsArr = text.trim().split(' ');
+    const count = wordsArr.filter(word => word !== '').length;
+    setWordCount(count);
+  }
 
   return (
     <div>
       <h1>How fast can you type?</h1>
-      <textarea name="input" onChange={handleInputChange} />
+      <textarea value={text} onChange={handleChange} />
       <h4>Time Remaining:</h4>
-      <button>Start</button>
-      <h1>Word Count:</h1>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
