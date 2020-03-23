80d08c528ef8c496a18605dcecc543b736fb1a7e
Paul Tobin <tobster111@gmail.com>
2020-03-23 15:24:02 +0000

    Number of words is calculated once the timer reaches 0

--- speedtyping/src/App.js
+++ speedtyping/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   const [text, setText] = useState('');
   const [wordCount, setWordCount] = useState(0);
-  const [timeRemaining, setTimeRemaining] = useState(15);
-  const [timer, setTimer] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(3);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -14,31 +14,28 @@ function App() {
   }
 
   function calculateWordCount(text) {
-    console.log(text);
     const wordsArr = text.trim().split(' ');
     const count = wordsArr.filter(word => word !== '').length;
-    setWordCount(count);
+    return count;
   }
 
   useEffect(() => {
-    let id;
-    if (timer && timeRemaining > 0) {
-      id = setTimeout(() => {
-        setTimeRemaining(timeRemaining - 1);
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1);
       }, 1000);
-    } else if (timer === 0) {
-      setTimer(false);
+    } else if (timeRemaining === 0) {
+      setIsTimeRunning(false);
+      setWordCount(calculateWordCount(text));
     }
-
-    return () => clearTimeout(id);
-  }, [timeRemaining, timer]);
+  }, [timeRemaining, isTimeRunning, text]);
 
   return (
     <div>
       <h1>How fast can you type?</h1>
       <textarea value={text} onChange={handleChange} />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={() => setTimer(true)}>Start</button>
+      <button onClick={() => setIsTimeRunning(true)}>Start</button>
       <h1>Word Count: {wordCount}</h1>
     </div>
   );
