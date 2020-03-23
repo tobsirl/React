e11c944c751aaa82055a8f12516e1e7f5b908ee1
Paul Tobin <tobster111@gmail.com>
2020-03-23 14:32:15 +0000

    Start button now starts the timer

--- speedtyping/src/App.js
+++ speedtyping/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
   const [text, setText] = useState('');
   const [wordCount, setWordCount] = useState(0);
-  const [countDown, setCountDown] = useState(15);
+  const [timeRemaining, setTimeRemaining] = useState(15);
+  const [timer, setTimer] = useState(false);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -20,21 +21,24 @@ function App() {
   }
 
   useEffect(() => {
-    const id = setTimeout(() => {
-      if (countDown === 0) return;
-      setCountDown(countDown - 1);
-    }, 1000);
-    return () => {
-      clearTimeout(id);
-    };
-  }, [countDown]);
+    let id;
+    if (timer && timeRemaining > 0) {
+      id = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+    } else if (timer === 0) {
+      setTimer(false);
+    }
+
+    return () => clearTimeout(id);
+  }, [timeRemaining, timer]);
 
   return (
     <div>
       <h1>How fast can you type?</h1>
       <textarea value={text} onChange={handleChange} />
-      <h4>Time Remaining: {countDown}</h4>
-      <button onClick={() => calculateWordCount(text)}>Start</button>
+      <h4>Time Remaining: {timeRemaining}</h4>
+      <button onClick={() => setTimer(true)}>Start</button>
       <h1>Word Count: {wordCount}</h1>
     </div>
   );
