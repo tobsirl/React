5f228929d52be898ade72d01a78507e1f8ad8785
Paul Tobin <tobster111@gmail.com>
2020-03-30 14:37:23 +0100

    Installing react-router-dom, basic routing with Switch, Route and Link

--- React Router Examples/react-router-practice/src/App.js
+++ React Router Examples/react-router-practice/src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+import Home from './Home';
+import Product from './Product.js';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+      </nav>
 
+      <Switch>
+        <Route exact path={`/`}>
+          <Home />
+        </Route>
+        <Route path={`/products`}>
+          <Product />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+// Route between 2 components depending on the path:
+// 1. Home page ("/")
+// 2. Products Page ("/products")
+
+// ** Home Page **
+// • Can be a basic home page, maybe welcoming the user to your site and giving some background about yourself as a developer
+
+// ** Products Page **
+// • Using an array of data for your products (provided for you), map over that data to show each item and some information on
+// the products page as a list of available products.
