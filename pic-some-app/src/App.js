d9122d54f9146a5909868ea6906a3e6db63cb733
Paul Tobin <tobster111@gmail.com>
2020-03-31 11:47:13 +0100

    Setup react-router-dom to display Photos and Cart

--- pic-some-app/src/App.js
+++ pic-some-app/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
+import { Switch, Route } from 'react-router-dom';
+
+import Photos from './pages/Photo';
+import Cart from './pages/Cart';
 
 import './App.css';
 
@@ -7,7 +11,15 @@ function App() {
   return (
     <div>
       <Header />
-      <h1>Home Page</h1>
+
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
   );
 }
