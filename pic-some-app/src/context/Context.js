0cacec72f19df291fe9073450afc39ce20f87b87
Paul Tobin <tobster111@gmail.com>
2020-03-31 13:10:41 +0100

    Using use

--- pic-some-app/src/context/Context.js
+++ pic-some-app/src/context/Context.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Context = React.createContext();
 
 function ContextProvider({ children }) {
-  return <Context.Provider value="">{children}</Context.Provider>;
+  const [allPhotos, setAllPhotos] = useState([]);
+
+  useEffect(() => {
+    (async function fetchData() {
+      const response = await fetch(
+        `https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json`
+      );
+      const photos = await response.json();
+      setAllPhotos(photos);
+    })();
+  }, []);
+
+  console.log(allPhotos);
+
+  return <Context.Provider value={{ allPhotos }}>{children}</Context.Provider>;
 }
 
 export { ContextProvider, Context };
