f0964ac2d746d4c0968037c0c23d84ec2c1bbea6
Paul Tobin <tobster111@gmail.com>
2020-04-01 09:44:05 +0100

    Map over the photos and display them on the page

--- pic-some-app/src/pages/Photo.js
+++ pic-some-app/src/pages/Photo.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../context/Context';
 
 function Photos() {
+  const { allPhotos } = useContext(Context);
+
+  const photos = allPhotos.map(({id, url}) => {
+    return <img key={id} src={url} alt="" />;
+  });
   return (
     <main className="photos">
       <h1>Images go here</h1>
+      <ul>{photos}</ul>
     </main>
   );
 }
