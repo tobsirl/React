0863ce8fdb1d1c4fa1c5d90df283eea543c49ddb
Paul Tobin <tobster111@gmail.com>
2020-04-05 11:54:48 +0100

    Changing the cart icon to fill if items are in the cart

--- pic-some-app/src/components/Header.js
+++ pic-some-app/src/components/Header.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../context/Context';
 
 function Header() {
+  const { cartItems } = useContext(Context);
+  console.log(cartItems.length);
+
+  const cartFull =
+    cartItems.length > 0 ? (
+      <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
+    ) : (
+      <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+    );
+
   return (
     <header>
       <Link to="/">
         <h2>Pic Some</h2>
       </Link>
-      <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
-      </Link>
+      <Link to="/cart">{cartFull}</Link>
     </header>
   );
 }
