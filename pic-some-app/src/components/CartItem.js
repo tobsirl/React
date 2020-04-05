fa08a4abb34b7863fc64152991f96d6e1f880e16
Paul Tobin <tobster111@gmail.com>
2020-04-05 12:55:50 +0100

    Removing an item from the cart using the delete button. onClick on the delete icon passing item.id to removeCartItem

--- pic-some-app/src/components/CartItem.js
+++ pic-some-app/src/components/CartItem.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../context/Context';
 
 function CartItem({ item }) {
+  const { removeCartItem } = useContext(Context);
+
   return (
     <div className="cart-item">
-      <i className="ri-delete-bin-line"></i>
+      <i
+        className="ri-delete-bin-line"
+        onClick={() => removeCartItem(item.id)}
+      ></i>
       <img src={item.url} width="130px" alt="" />
       <p>$5.99</p>
     </div>
