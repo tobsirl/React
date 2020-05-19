e3eb8108987f13239b545b185d53c05c74c53780
Paul Tobin <tobster111@gmail.com>
2020-05-19 19:56:50 +0100

    Styling the app

---
+++ react-movie-search/src/components/SearchMovies.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function SearchMovies() {
+  return (
+    <div>
+      <form className="form">
+        <label htmlFor="query" className="label">
+          Movie Name
+        </label>
+        <input
+          type="text"
+          name="query"
+          className="input"
+          placeholder="The Godfather"
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
