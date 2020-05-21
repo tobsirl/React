49ee34031c8ad47281f1722a78ea355d638f41ae
Paul Tobin <tobster111@gmail.com>
2020-05-21 19:12:12 +0100

    Controlled input and a search movie function

--- react-movie-search/src/components/SearchMovies.js
+++ react-movie-search/src/components/SearchMovies.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchMovies() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  async function searchMovie(e) {
+    e.preventDefault();
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=4e6a1064e598fc7775b77627eaf33da0&query=${searchTerm}`
+    );
+
+    const data = await response.json();
+
+    return data;
+  }
+
   return (
     <div>
       <form className="form">
@@ -12,8 +25,9 @@ export default function SearchMovies() {
           name="query"
           className="input"
           placeholder="The Godfather"
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className="button" type="submit">
+        <button className="button" type="submit" onClick={searchMovie}>
           Search
         </button>
       </form>
