a97c8064f3919d039f96a7601364e83da08d4126
Paul Tobin <tobster111@gmail.com>
2020-05-25 20:12:15 +0100

    Styling the card component

--- react-movie-search/src/components/MoviesCardList.js
+++ react-movie-search/src/components/MoviesCardList.js
@@ -2,10 +2,37 @@ import React from 'react';
 
 export default function MoviesCardList(props) {
   return (
-    <ul>
-      {props.movies.map(({id, title}) => (
-        <li key={id}>{title}</li>
-      ))}
-    </ul>
+    <div className="card__list__item">
+      {props.movies
+        .filter(({ poster_path }) => poster_path)
+        .map(
+          ({
+            id,
+            title,
+            poster_path,
+            release_date,
+            vote_average,
+            overview,
+          }) => (
+            <div className="card" key={id}>
+              <img
+                className="card__image"
+                src={`http://image.tmdb.org/t/p/w185/${poster_path}`}
+                alt="title"
+              />
+              <div class="card__content">
+                <h1 className="card__title">{title}</h1>
+                <p>
+                  <small>RELEASE DATE: {release_date}</small>
+                </p>
+                <p>
+                  <small>RATING: {vote_average}</small>
+                </p>
+                <p class="card__description">{overview.slice(0, 50) + '...'}</p>
+              </div>
+            </div>
+          )
+        )}
+    </div>
   );
 }
