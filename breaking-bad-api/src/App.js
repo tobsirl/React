3dfb556102b93f575d7e52b40a967d25ee973fae
Paul Tobin <tobster111@gmail.com>
2020-07-05 14:26:08 +0100

    Setup the header component, loading the data from the breaking bad api

--- breaking-bad-api/src/App.js
+++ breaking-bad-api/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import Header from './components/ui/Header';
+
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const response = await fetch(`https://www.breakingbadapi.com/api/characters`);
+      const result = await response.json();
+      setCharacters(result)
+    };
+    fetchCharacters()
+  }, []);
+
+  console.log(characters);
   return (
+    
     <div className="container">
-      
+      <Header />
     </div>
   );
 }
