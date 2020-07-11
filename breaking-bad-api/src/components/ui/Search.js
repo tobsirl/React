5274d7207a02d5cbbbd746fcfb8196ae4ce2f5bf
Paul Tobin <tobster111@gmail.com>
2020-07-11 15:28:05 +0100

    Made search a controlled component, value={search}, onChange={}

--- breaking-bad-api/src/components/ui/Search.js
+++ breaking-bad-api/src/components/ui/Search.js
@@ -1,14 +1,22 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 
 const Search = () => {
+  const [search, setSearch] = useState('');
+
   return (
     <section className="search">
-    <form>
-      <input type="text" className="form-control" placeholder="Search characters"/>
-    </form>
-      
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search characters"
+          autoFocus
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
