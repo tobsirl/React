ffb19dc4305c852c73a49011058df17fc7c6b406
Paul Tobin <tobster111@gmail.com>
2021-01-30 16:24:50 +0000

    Styling on the boxes

--- custom-styled-components/src/App.js
+++ custom-styled-components/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import './box-styles.css';
 
-const smallBox = <div>small lightblue box</div>;
-const mediumBox = <div>medium pink box</div>;
-const largeBox = <div>large orange box</div>;
+const smallBox = <div className="box box--small">small lightblue box</div>;
+const mediumBox = <div className="box box--medium">medium pink box</div>;
+const largeBox = <div className="box box--large">large orange box</div>;
 
 function App() {
   return (
