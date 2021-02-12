620223c653ddf791c9f09e425882d65f0f759c5c
Paul Tobin <tobster111@gmail.com>
2021-02-12 12:07:01 +0000

    Added the display component

--- lifting-state-2/src/App.js
+++ lifting-state-2/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+import Display from './components/Display';
+
 function App() {
   return (
     <div className="App">
       <h1>display</h1>
+      <Display name="Paul" dogsName="rex" />
     </div>
   );
 }
---
+++ lifting-state-2/src/components/Display.jsx
@@ -0,0 +1,3 @@
+export default function Display({ name, dogsName }) {
+  return `My name is ${name} and my dog's name is ${dogsName}`;
+}
