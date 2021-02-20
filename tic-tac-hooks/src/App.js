c2866e29c1d657678f7c8a1b628f110da353ba46
Paul Tobin <tobster111@gmail.com>
2021-02-20 14:53:33 +0000

    Added the game board

--- tic-tac-hooks/src/App.js
+++ tic-tac-hooks/src/App.js
@@ -1,8 +1,39 @@
 import './App.css';
 
 function Board() {
+  const squares = Array(9).fill(null)
+
+
+  function selectSquare(square) {
+
+  }
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => selectSquare(i)}>
+        {squares[i]}
+      </button>
+    )
+  }
   return (
-    <h1>board</h1>
+    <div>
+      <div class="status">{}</div>
+      <div class="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div class="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div class="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
   )
 }
 
