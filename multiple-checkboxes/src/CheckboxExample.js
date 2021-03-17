be3dd93ee6d5f0c252817a30590552cf736720f0
Paul Tobin <tobster111@gmail.com>
2021-03-17 10:31:20 +0000

    Setup of multiple checkbox example

---
+++ multiple-checkboxes/src/CheckboxExample.js
@@ -0,0 +1,7 @@
+import './App.css';
+
+function CheckboxExample() {
+  return <div className="App">test</div>;
+}
+
+export default CheckboxExample;
---
+++ multiple-checkboxes/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CheckboxExample from './CheckboxExample';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CheckboxExample />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
