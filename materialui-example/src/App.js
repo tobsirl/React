ee4dcc09750881ad3f576c27b03f623acbaa1e52
Paul Tobin <tobster111@gmail.com>
2021-03-27 15:04:34 +0000

    Added Create and Notes

--- materialui-example/src/App.js
+++ materialui-example/src/App.js
@@ -1,9 +1,19 @@
-
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import Create from './pages/Create';
 
 function App() {
   return (
-   <div>App</div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
