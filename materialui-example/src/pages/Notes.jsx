14ff3cfc97f123adc2c0753ca8ae0824ef21b0be
Paul Tobin <tobster111@gmail.com>
2021-04-01 20:13:01 +0100

    Getting the notes from json-server

--- materialui-example/src/pages/Notes.jsx
+++ materialui-example/src/pages/Notes.jsx
@@ -1,3 +1,18 @@
+import React, { useState, useEffect } from 'react';
 export default function Notes() {
-  return <div>Notes</div>;
+  const [notes, setNotes] = useState([]);
+
+  async function fetchNotes() {
+    const response = await fetch('http://localhost:8000/notes');
+
+    const data = await response.json();
+
+    setNotes(data);
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return <div>{JSON.stringify(notes[0].title, null, 2)}</div>;
 }
