28cee14fd9c8a883353a803907cdbded21c605c9
Paul Tobin <tobster111@gmail.com>
2021-04-07 19:47:24 +0100

    Setup simple counter

---
+++ usereducer-example/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react'
+
+
+function Counter() {
+  
+}
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
