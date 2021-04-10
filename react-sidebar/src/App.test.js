790d75c802d11c892ad336c5bb4bd5505922e081
Paul Tobin <tobster111@gmail.com>
2021-04-10 09:46:06 +0100

    Setup the files and installed react-router

--- react-sidebar/src/App.test.js
+++ react-sidebar/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/react sidebart/i);
+  const linkElement = screen.getByText(/react sidebar/i);
   expect(linkElement).toBeInTheDocument();
 });
