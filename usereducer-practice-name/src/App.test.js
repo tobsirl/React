fcbf555128505c1338e6d65ee5eefd9c325d53fe
Paul Tobin <tobster111@gmail.com>
2021-04-11 16:58:25 +0100

    Adding test using userEvent

--- usereducer-practice-name/src/App.test.js
+++ usereducer-practice-name/src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const button = screen.getByRole('button', {  name: /change name/i})
+  const button = screen.getByRole('button', { name: /change name/i })
   expect(button).toBeInTheDocument()
 })
 
@@ -11,4 +12,16 @@ test('should check for the name', () => {
   render(<App />)
   const text = screen.getByText(/name: paul/i)
   expect(text).toBeInTheDocument()
-});
+})
+
+test('should click the button and check the result', () => {
+  // arrange
+  render(<App />)
+
+  // act
+  userEvent.click(screen.getByText('Change Name'))
+  const text = screen.getByText(/name: simon/i)
+
+  // assert
+  expect(text).toBeInTheDocument()
+})
