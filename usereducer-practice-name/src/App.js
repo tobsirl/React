5d60f4b523df94b1ddfddacd6eb0eaa6bcb09e2c
Paul Tobin <tobster111@gmail.com>
2021-04-11 16:37:20 +0100

    Change name

--- usereducer-practice-name/src/App.js
+++ usereducer-practice-name/src/App.js
@@ -1,9 +1,40 @@
+import React, { useReducer } from 'react'
 import './App.css'
+
+function nameReducer(state, action) {
+  console.log(state.name)
+  switch (action.type) {
+    case 'SIMON': {
+      return {
+        name: state.name = 'Simon',
+      }
+    }
+
+    default:
+      throw new Error(`Unsupported action: ${action.type}`)
+  }
+}
+
+function Names({ initialName = 'Paul' }) {
+  const [state, dispatch] = useReducer(nameReducer, { name: initialName })
+
+  function nameChangeSimon() {
+    dispatch({ type: 'SIMON' })
+  }
+
+  const { name } = state
+  return (
+    <div>
+      <button onClick={nameChangeSimon}>{name}</button>
+      <p></p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Names />
     </div>
   )
 }
