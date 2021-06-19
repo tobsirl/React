a802271c55f5b6c65577123692784510bf8c8d07
Paul Tobin <tobster111@gmail.com>
2021-06-19 20:21:27 +0100

    Added Dashboard and LoginScreen

--- composition/src/App.js
+++ composition/src/App.js
@@ -9,12 +9,36 @@ function Header() {
   );
 }
 
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+function LoginScreen({ onLogin }) {
+  return (
+    <div>
+      <h1>LoginScreen</h1>
+      <button onClick={onLogin}>Login</button>
+    </div>
+  );
+}
+
 function App() {
   const [currentUser, setCurrentUser] = useState('');
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <div style={{ backgroundColor: 'lightblue' }}>
         <Header />
+      </div>
+      <div style={{ flex: 1 }}>
+        {currentUser ? (
+          <Dashboard />
+        ) : (
+          <LoginScreen onLogin={() => setCurrentUser({ name: 'Paul' })} />
+        )}
       </div>
     </div>
   );
