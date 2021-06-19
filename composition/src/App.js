9a7468d99074a68acde7a3ab398c195fc78d4e20
Paul Tobin <tobster111@gmail.com>
2021-06-19 20:12:22 +0100

    Example on composition to solve prop drilling

---
+++ composition/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [currentUser, setCurrentUser] = useState('');
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
