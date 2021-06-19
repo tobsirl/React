cc0991ad017b4fad1cda014a77e3664fa4eb8ff3
Paul Tobin <tobster111@gmail.com>
2021-06-19 20:15:44 +0100

    Added the header component

--- composition/src/App.js
+++ composition/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+}
+
 function App() {
   const [currentUser, setCurrentUser] = useState('');
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <div style={{ backgroundColor: 'lightblue' }}>
+        <Header />
+      </div>
     </div>
   );
 }
--- usereducer-example/src/App.js
+++ usereducer-example/src/App.js
@@ -2,16 +2,27 @@ import './App.css';
 import React, { useReducer } from 'react';
 
 function countReducer(state, action) {
-  console.log('action :>> ', action);
-  return state + action;
+  console.log('state :>> ', state.count);
+  console.log('action :>> ', action.step);
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.step
+      }
+      
+    default:
+      throw new Error('Not supported');
+  }
 }
 
-function Counter({ step = 1, initialCount = 0 }) {
-  const [count, changeCount] = useReducer(countReducer, initialCount);
+function Counter({ step = 3, initialCount = 0 }) {
+  const [state, dispatch] = useReducer(countReducer, { count: initialCount });
 
   function increment() {
-    changeCount(step);
+    dispatch({ type: 'INCREMENT', step: step });
   }
+
+  const { count } = state;
 
   return (
     <div>
