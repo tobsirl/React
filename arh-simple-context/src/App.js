ff434e9da28473a39959b0d005e3e603c6ea6f02
Paul Tobin <tobster111@gmail.com>
2021-06-21 20:43:27 +0100

    Using useCount to flag errors

--- arh-simple-context/src/App.js
+++ arh-simple-context/src/App.js
@@ -29,14 +29,14 @@ function useCount() {
 
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const [count] = React.useContext(CountContext);
+  const [count] = useCount();
   return <div>{`The current count is ${count}`}</div>;
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
 
-  const [, setCount] = React.useContext(CountContext);
+  const [, setCount] = useCount();
   const increment = () => setCount((c) => c + 1);
   return <button onClick={increment}>Increment count</button>;
 }
