e3acbadd7c7ed3e0363c9eaa53231b35715dbfc7
Paul Tobin <tobster111@gmail.com>
2021-06-25 16:40:56 +0100

    Using formatDebugValue

--- use-debug-value/src/App.js
+++ use-debug-value/src/App.js
@@ -4,11 +4,13 @@
 import * as React from 'react';
 import './App.css';
 
+const formatDebugValue = ({ query, state }) => `\`${query}\` => ${state}`;
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState);
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
-  React.useDebugValue(`\`${query}\` => ${state}`);
+  React.useDebugValue({ query, state }, formatDebugValue);
 
   React.useEffect(() => {
     let mounted = true;
