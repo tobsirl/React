100e34d3c1f61f5940f4bd7447acba843ff50568
Paul Tobin <tobster111@gmail.com>
2021-06-25 16:38:52 +0100

    Using useDebugValue

--- use-debug-value/src/App.js
+++ use-debug-value/src/App.js
@@ -8,6 +8,7 @@ function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState);
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
+  React.useDebugValue(`\`${query}\` => ${state}`);
 
   React.useEffect(() => {
     let mounted = true;
