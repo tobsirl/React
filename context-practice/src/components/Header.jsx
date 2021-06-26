4dd2cf3ebff78b4a5b1d7845f0097c308bcc2563
Paul Tobin <tobster111@gmail.com>
2021-06-26 17:51:18 +0100

    Change the header

--- context-practice/src/components/Header.jsx
+++ context-practice/src/components/Header.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+
 function Header() {
+  const value = React.useContext(ThemeContext);
   return (
-    <header className="light-theme">
-      <h2>Light Theme</h2>
+    <header className={`${value}-theme`}>
+      <h2>{value.slice(0, 1).toUpperCase() + value.slice(1)} Theme</h2>
     </header>
   );
 }
--- context-practice/src/context/ThemeContext.js
+++ context-practice/src/context/ThemeContext.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const ThemeContext = React.createContext();
 export default function ThemeProvider(props) {
-  return <ThemeContext.Provider value="dark" {...props} />;
+  return <ThemeContext.Provider value="light" {...props} />;
 }
 
 export { ThemeContext };
