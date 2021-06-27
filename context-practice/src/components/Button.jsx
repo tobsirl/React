34f3492faf46e781c290f78181785a179b193687
Paul Tobin <tobster111@gmail.com>
2021-06-27 12:48:46 +0100

    Using setTheme to change the color of the theme

--- context-practice/src/components/Button.jsx
+++ context-practice/src/components/Button.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { ThemeContext } from '../context/ThemeContext'
 
 function Button() {
-  const [theme] = React.useContext(ThemeContext)
-  console.log(theme)
-  return <button className={`${theme}-theme`}>Switch Theme</button>
+  const [theme, setTheme] = React.useContext(ThemeContext)
+  return (
+    <button className={`${theme}-theme`} onClick={() => setTheme('dark')}>
+      Switch Theme
+    </button>
+  )
 }
 
 export default Button
