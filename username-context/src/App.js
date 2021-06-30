e5d2dc8ecc2fd2ee8f15afb71e62b6c43bbbf5e3
Paul Tobin <tobster111@gmail.com>
2021-06-30 19:54:06 +0100

    Added Header and App

--- username-context/src/App.js
+++ username-context/src/App.js
@@ -1,11 +1,47 @@
-import './App.css';
+import React from "react"
+
+import Header from "./Header"
+import UserContext from "./userContext"
 
 function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                <UserContext.Consumer>
+                    {username => (
+                        <p className="main">No new notifications, {username}! 🎉</p>
+                    )}
+                </UserContext.Consumer>
+            </main>
+            
+            {
+                /**
+                 * Challenge: Add the ability for the user to choose a new username
+                 * 
+                 * 1. Add state to this component to hold the new username in a controlled form
+                 * (https://reactjs.org/docs/forms.html#controlled-components)
+                 * (https://scrimba.com/p/p7P5Hd/cW8Jdfy)
+                 * 
+                 * 2. Change userContext into a component that has state and provides the ability
+                 * to change the user's username. Make sure to export the provider and consumer
+                 * as named exports and update their uses anywhere else in the app
+                 * 
+                 * 3. Give this App component the ability to update the username in context when the
+                 * button is clicked
+                 */
+            }
+            <input
+                type="text"
+                name="username"
+                placeholder="New username"
+                value={???}
+                onChange={???}
+            />
+            <button onClick={???}>Change Username</button>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
