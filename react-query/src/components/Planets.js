2cdde2d89bbfe37b785c9a5c0e9b4082cf975acf
Paul Tobin <tobster111@gmail.com>
2021-07-09 15:36:54 +0100

    Using react query to fetch data

--- react-query/src/components/Planets.js
+++ react-query/src/components/Planets.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 
+async function fetchPlanets() {
+  const response = await fetch('http://swapi.dev/api/planets/')
+  return response.json()
+}
 export default function Planets() {
+  const { data, status } = useQuery('planets', fetchPlanets)
   return (
     <div>
       <h2>Planets</h2>
