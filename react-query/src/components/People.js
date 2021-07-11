6f6568ace13d573d07ab8a5c72f58eec5d497bbe
Paul Tobin <tobster111@gmail.com>
2021-07-11 10:20:19 +0100

    Refactored Planets to People

--- react-query/src/components/People.js
+++ react-query/src/components/People.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 
-export default function People() {
+import Person from './Person'
+
+async function fetchPeople() {
+  const response = await fetch('http://swapi.dev/api/people/')
+  return response.json()
+}
+export default function Planets() {
+  const { isLoading, isError, data, error } = useQuery('people', fetchPeople)
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (isError) {
+    return <p>{`Error Message: ${error}`}</p>
+  }
+
   return (
     <div>
       <h2>People</h2>
+      <div>
+        {data.results.map((person) => (
+          <Person key={person.name} {...person} />
+        ))}
+      </div>
     </div>
   )
 }
