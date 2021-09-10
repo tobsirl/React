17772711ff10afd37b64a8b24a6f2f3db92b785b
Paul Tobin <tobster111@gmail.com>
2021-09-10 17:36:18 +0100

    Passing props

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import { render } from 'react-dom';
-
 function Pet() {
   return React.createElement('div', {}, [
     React.createElement('h2', {}, 'Luna'),
@@ -11,11 +8,23 @@ function Pet() {
 
 function App() {
   return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Sudo',
+      animal: 'Dog',
+      breed: 'Wheaten Terrier',
+    }),
   ]);
 }
 
-render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
