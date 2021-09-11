7e3ec70c556c369dc44730ad4b4562155b459d95
Paul Tobin <tobster111@gmail.com>
2021-09-11 16:45:09 +0100

    Changed App.js to jsx

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -3,24 +3,14 @@ import { render } from 'react-dom'
 import Pet from './Pet'
 
 function App() {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese',
-    }),
-    React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cockatiel',
-    }),
-    React.createElement(Pet, {
-      name: 'Sudo',
-      animal: 'Dog',
-      breed: 'Wheaten Terrier',
-    }),
-  ])
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  )
 }
 
 render(React.createElement(App), document.getElementById('root'))
