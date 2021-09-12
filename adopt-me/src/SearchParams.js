ec9b0b79ce5f418111463b8bb76e528e36284bbb
Paul Tobin <tobster111@gmail.com>
2021-09-12 10:15:32 +0100

    Data binding

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,5 +1,8 @@
+import { useState } from 'react'
+
 export default function SearchParams() {
-  const location = 'Seatle, WA'
+  const [location, setLocation] = useState('Seattle, WA')
+
   return (
     <div className="search-params">
       <form action="">
@@ -10,6 +13,7 @@ export default function SearchParams() {
             id="location"
             value={location}
             placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
           />
         </label>
         <button>Submit</button>
