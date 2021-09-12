891bc5d71d7eef73ac60cefbdf5b82d4c294f3a8
Paul Tobin <tobster111@gmail.com>
2021-09-12 13:56:47 +0100

    Creating the results page

---
+++ adopt-me/src/Results.js
@@ -0,0 +1,20 @@
+import Pet from './Pet'
+
+export default function Results({ pets }) {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No Pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  )
+}
