ec4db1407052ec364f33d0df434f75ef8651c008
Paul Tobin <tobster111@gmail.com>
2021-09-12 10:42:04 +0100

    Add animal accept

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
 export default function SearchParams() {
   const [location, setLocation] = useState('Seattle, WA')
+  const [animal, setAnimal] = useState('')
 
   return (
     <div className="search-params">
@@ -15,6 +17,21 @@ export default function SearchParams() {
             placeholder="Location"
             onChange={(event) => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="Animal"
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.vaule)}
+            onBlur={(event) => setAnimal(event.target.vaule)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
