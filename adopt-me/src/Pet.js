2fa73b68cc09b24f83a908e9fd0015f804666d9e
Paul Tobin <tobster111@gmail.com>
2021-09-12 14:10:39 +0100

    Added classname info

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -4,14 +4,14 @@ function Pet({ name, animal, breed, location, id, images }) {
   if (images.length) {
     hero = images[0]
   }
-  
+
   return (
     <a href={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
 
-      <div>
+      <div className="info">
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
