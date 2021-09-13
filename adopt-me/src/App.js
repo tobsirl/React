49a9c2fa0ecc3161e5dd2c0146735b8b97fb358a
Paul Tobin <tobster111@gmail.com>
2021-09-13 19:50:48 +0100

    Using switch

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,22 +1,24 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import Details from './Details'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import SearchParams from './SearchParams'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>Adopt Me!</h1>
-        <Route path="/search">
-          <SearchParams />
-        </Route>
-        <Route path="/details">
-          <Details />
-        </Route>
-      </div>
-    </Router>
+    <div>
+      <h1>Adopt Me!</h1>
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
