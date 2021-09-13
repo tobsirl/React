35534a87266c84484909427867811bfac6203ee0
Paul Tobin <tobster111@gmail.com>
2021-09-13 20:19:40 +0100

    Fetching the data with componentDidMount

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -2,7 +2,22 @@ import { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
 class Details extends Component {
+  constructor() {
+    super()
+    this.state = { loading: true }
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`,
+    )
+
+    const json = res.json()
+    this.setState(Object.assign({ loading: false }, json.pets[0]))
+  }
+
   render() {
+    console.log(this.state.loading)
     return (
       <div>
         <h2>hi</h2>
@@ -11,4 +26,4 @@ class Details extends Component {
   }
 }
 
-export default Details
+export default withRouter(Details)
