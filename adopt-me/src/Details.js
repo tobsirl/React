1d5ddc8a50d010b6e981546f4ce732fbbe3bb82f
Paul Tobin <tobster111@gmail.com>
2021-09-13 20:06:31 +0100

    Changed Details to a class component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,7 +1,14 @@
-export default function Details() {
-  return (
-    <div>
-      <h2>hi</h2>
-    </div>
-  )
+import { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+class Details extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hi</h2>
+      </div>
+    )
+  }
 }
+
+export default Details
