1871845ae61a46ebca2b5b2b50ff04221b158592
Paul Tobin <tobster111@gmail.com>
2021-09-13 19:53:48 +0100

    Using Link in Pets

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 function Pet({ name, animal, breed, location, id, images }) {
   let hero = 'http://pets-images.dev-apis.com/pets/none.jpg'
 
@@ -6,7 +8,7 @@ function Pet({ name, animal, breed, location, id, images }) {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -15,7 +17,7 @@ function Pet({ name, animal, breed, location, id, images }) {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   )
 }
 
