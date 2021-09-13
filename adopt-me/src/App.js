90924562f058fc8c7d5660664b14a7b37a1d22f2
Paul Tobin <tobster111@gmail.com>
2021-09-13 19:44:33 +0100

    Setup react router

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
+import Details from './Details'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import SearchParams from './SearchParams'
 
 function App() {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <Router>
+      <div>
+        <h1>Adopt Me!</h1>
+        <Route path="/search">
+          <SearchParams />
+        </Route>
+        <Route path="/details">
+          <Details />
+        </Route>
+      </div>
+    </Router>
   )
 }
 
