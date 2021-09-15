232a77e49a209ea2a0590001339fbf3bfc51f19f
Paul Tobin <tobster111@gmail.com>
2021-09-15 18:42:49 +0100

    Wrapping Details in the ErrorBoundary

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import Carousel from './Carousel'
+import ErrorBoundary from './ErrorBoundary'
 
 class Details extends Component {
   state = { loading: true }
@@ -35,4 +36,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details)
+const DetailsWithRouter = withRouter(Details)
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  )
+}
