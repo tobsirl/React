77cdf7f5a4e65ee20217f59a69abe2b91f8119fd
Paul Tobin <tobster111@gmail.com>
2021-09-16 19:44:12 +0100

    Model finished

--- adopt-me/src/Modal.js
+++ adopt-me/src/Modal.js
@@ -6,7 +6,18 @@ const modalRoot = document.getElementById('modal')
 function Modal({ children }) {
   const elRef = useRef(null)
 
-  if (elRef.current) {
+  if (!elRef.current) {
     elRef.current = document.createElement('div')
   }
+
+  useEffect(() => {
+    modalRoot.appendChild(elRef.current)
+    return () => {
+      modalRoot.removeChild(elRef.current)
+    }
+  }, [])
+
+  return createPortal(<div>{children}</div>, elRef.current)
 }
+
+export default Modal
