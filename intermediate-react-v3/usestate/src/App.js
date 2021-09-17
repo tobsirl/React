3ac861c8cc2174e0caf05deae919352a808a3f87
Paul Tobin <tobster111@gmail.com>
2021-09-17 17:06:05 +0100

    Setup of basic layout

---
+++ intermediate-react-v3/usestate/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>test</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
