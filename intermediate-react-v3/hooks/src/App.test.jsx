455be5b307b1fe366f4c458e434a858149c312f4
Paul Tobin <tobster111@gmail.com>
2021-09-18 11:55:57 +0100

    Tests for the useEffect component

--- intermediate-react-v3/hooks/src/App.test.jsx
+++ intermediate-react-v3/hooks/src/App.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import State from './State'
+import Effect from './Effect'
 
 describe('test for the <State /> component', () => {
   it('should check for the "useState Example" text', () => {
@@ -9,5 +10,15 @@ describe('test for the <State /> component', () => {
     const stateText = screen.getByText(/usestate example/i)
 
     expect(stateText).toHaveTextContent(/usestate example/i)
+  })
+})
+
+describe('test for the <Effect /> component', () => {
+  it('should check for the "useEffect Example" text', () => {
+    render(<Effect />)
+
+    const effectText = screen.getByText(/useeffect example/i)
+
+    expect(effectText).toHaveTextContent(/useeffect example/i)
   })
 })
