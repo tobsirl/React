55c20f88c8ba9d557ca4f83ed394b902b5186497
Paul Tobin <tobster111@gmail.com>
2021-09-18 11:06:02 +0100

    Added the State component

--- intermediate-react-v3/hooks/src/App.jsx
+++ intermediate-react-v3/hooks/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import React from 'react'
-
+import State from './State'
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <State />
     </div>
   )
 }
