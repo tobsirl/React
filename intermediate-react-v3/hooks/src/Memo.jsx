2c1dbe364cc19348cd9f7734c1fb84720407fb5e
Paul Tobin <tobster111@gmail.com>
2021-09-20 19:52:11 +0100

    Adding Memo

--- intermediate-react-v3/hooks/src/Memo.jsx
+++ intermediate-react-v3/hooks/src/Memo.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState, useMemo } from 'react'
 
+function fibonacci(n) {
+  if (n <= 1) {
+    return 1
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
 export default function Memo() {
+  const [num, setNum] = useState(1)
+  const [isGreen, setIsGreen] = useState(true)
+  const fib = useMemo(() => fibonacci(num), [num])
+
   return (
     <div>
-      <h1>Memo</h1>
+      <h1
+        onClick={() => setIsGreen(!isGreen)}
+        style={{ color: isGreen ? 'limegreen' : 'crimson' }}
+      >
+        useMemo Example
+      </h1>
+      <h2>
+        Fibonacci of {num} is {fib}
+      </h2>
+      <button type="button" onClick={() => setNum(num + 1)}>
+        ➕
+      </button>
     </div>
   )
 }
