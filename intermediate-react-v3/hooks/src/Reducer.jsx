7612684eda8ac0ea79116993c3e7d120a303f492
Paul Tobin <tobster111@gmail.com>
2021-09-20 19:05:37 +0100

    Added the body

--- intermediate-react-v3/hooks/src/Reducer.jsx
+++ intermediate-react-v3/hooks/src/Reducer.jsx
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react'
 
+// eslint-disable-next-line no-nested-ternary
 const limitRGB = (num) => (num < 0 ? 0 : num > 255 ? 255 : num)
 
 const step = 50
@@ -23,9 +24,38 @@ function reducer(state, action) {
   }
 }
 export default function Reducer() {
+  const [{ r, g, b }, dispatch] = useReducer(reducer, { r: 0, g: 0, b: 0 })
+
   return (
     <div>
-      <h1>Reducer</h1>
+      <h1 style={{ color: `rgb(${r}, ${g}, ${b})` }}>useReducer Example</h1>
+      <div>
+        <span>r</span>
+        <button type="button" onClick={() => dispatch({ type: 'INCREMENT_R' })}>
+          ➕
+        </button>
+        <button type="button" onClick={() => dispatch({ type: 'DECREMENT_R' })}>
+          ➖
+        </button>
+      </div>
+      <div>
+        <span>g</span>
+        <button type="button" onClick={() => dispatch({ type: 'INCREMENT_G' })}>
+          ➕
+        </button>
+        <button type="button" onClick={() => dispatch({ type: 'DECREMENT_G' })}>
+          ➖
+        </button>
+      </div>
+      <div>
+        <span>b</span>
+        <button type="button" onClick={() => dispatch({ type: 'INCREMENT_B' })}>
+          ➕
+        </button>
+        <button type="button" onClick={() => dispatch({ type: 'DECREMENT_B' })}>
+          ➖
+        </button>
+      </div>
     </div>
   )
 }
