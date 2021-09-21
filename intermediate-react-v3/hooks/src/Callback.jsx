9d798440859efc7334db21db0456ac8ee1631a66
Paul Tobin <tobster111@gmail.com>
2021-09-21 19:27:19 +0100

    Adding ExpensiveComputationComponent

--- intermediate-react-v3/hooks/src/Callback.jsx
+++ intermediate-react-v3/hooks/src/Callback.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useState, useEffect, useCallback, memo } from 'react'
+
+const ExpensiveComputationComponent = memo(({ compute, count }) => (
+  <div>
+    <h1>computed: {compute(count)}</h1>
+    <h4>last re-render {new Date().toLocaleTimeString()}</h4>
+  </div>
+))
 
 export default function Callback() {
   return (
