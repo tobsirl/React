15b7c85a86afb819fa08e29a95b41a94d22c2c85
Paul Tobin <tobster111@gmail.com>
2021-09-24 15:19:28 +0100

    Added ImperativeHandle component

--- intermediate-react-v3/hooks/src/ImperativeHandle.jsx
+++ intermediate-react-v3/hooks/src/ImperativeHandle.jsx
@@ -28,10 +28,55 @@ const ElaborateInput = forwardRef(
   },
 )
 
-export default function ImperativeHandle() {
+export default function ImperativeHandleComponent() {
+  const [city, setCity] = useState('Seattle')
+  const [state, setState] = useState('WA')
+  const [error, setError] = useState('')
+  const cityEl = useRef()
+  const stateEl = useRef()
+
+  function validate() {
+    // lol I found it on StackOverflow : https://stackoverflow.com/a/25677072
+    if (
+      !/^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]+$/.test(
+        city,
+      )
+    ) {
+      setError('city')
+      cityEl.current.focus()
+      return
+    }
+
+    if (!/^[A-Z]{2}$/.test(state)) {
+      setError('state')
+      stateEl.current.focus()
+      return
+    }
+
+    setError('')
+    alert('valid form!')
+  }
+
   return (
     <div>
-      <h1>useImperativeHandle Handle</h1>
+      <h1>useImperativeHandle Example</h1>
+      <ElaborateInput
+        hasError={error === 'city'}
+        placeholder="City"
+        value={city}
+        update={setCity}
+        ref={cityEl}
+      />
+      <ElaborateInput
+        hasError={error === 'state'}
+        placeholder="State"
+        value={state}
+        update={setState}
+        ref={stateEl}
+      />
+      <button type="button" onClick={validate}>
+        Validate Form
+      </button>
     </div>
   )
 }
