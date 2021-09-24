9e05426bdaeca77ff60c0b93ff27f5c068d3a435
Paul Tobin <tobster111@gmail.com>
2021-09-24 15:17:14 +0100

    Added ElaborateInput

--- intermediate-react-v3/hooks/src/ImperativeHandle.jsx
+++ intermediate-react-v3/hooks/src/ImperativeHandle.jsx
@@ -1,4 +1,32 @@
-import React from 'react'
+import React, { useState, useRef, useImperativeHandle, forwardRef } from 'react'
+
+const ElaborateInput = forwardRef(
+  ({ hasError, placeholder, value, update }, ref) => {
+    const inputRef = useRef()
+    useImperativeHandle(ref, () => ({
+      focus() {
+        inputRef.current.focus()
+      },
+    }))
+    return (
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={(e) => update(e.target.value)}
+        placeholder={placeholder}
+        style={{
+          padding: '5px 15px',
+          borderWidth: '3px',
+          borderStyle: 'solid',
+          borderColor: hasError ? 'crimson' : '#999',
+          borderRadius: '5px',
+          margin: '0 10px',
+          textAlign: 'center',
+        }}
+      />
+    )
+  },
+)
 
 export default function ImperativeHandle() {
   return (
