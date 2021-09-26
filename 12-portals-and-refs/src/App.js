2c7bad0ef899f7af7b18f349dc6d3e8a36d7703b
Paul Tobin <tobster111@gmail.com>
2021-09-26 15:52:13 +0100

    Styling the Adopt me

--- 12-portals-and-refs/src/App.js
+++ 12-portals-and-refs/src/App.js
@@ -18,7 +18,9 @@ const App = () => {
       >
         <Router>
           <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
-            <Link to="/">Adopt Me!</Link>
+            <Link className="text-6xl text-white hover:text-gray-200" to="/">
+              Adopt Me!
+            </Link>
           </header>
           <Switch>
             <Route path="/details/:id">
