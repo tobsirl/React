600014187b05e4450259d1a38a171d053f0f5e77
Paul Tobin <tobster111@gmail.com>
2021-09-27 09:48:37 +0100

    Styling the header and form

--- 12-portals-and-refs/src/SearchParams.js
+++ 12-portals-and-refs/src/SearchParams.js
@@ -27,8 +27,9 @@ const SearchParams = () => {
   }
 
   return (
-    <div>
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center divide-y divide-gray-900"
         onSubmit={(e) => {
           e.preventDefault();
           requestPets();
