d29945d3dc6c2af001a281ce866a6563496f63f3
Paul Tobin <tobster111@gmail.com>
2021-10-03 10:22:44 +0100

    Added the second test

--- testing-react/src/__tests__/Pet.test.jsx
+++ testing-react/src/__tests__/Pet.test.jsx
@@ -13,6 +13,18 @@ test("displays a default thumbnail", async () => {
   const petThumbnail = await screen.findByTestId("thumbnail");
 
   expect(petThumbnail.src).toContain("none.jpg");
+});
+
+test("displays a non-default, correct thumbnail", async () => {
+  render(
+    <StaticRouter>
+      <Pet images={["1.jpg", "2.jpg", "3.jpg"]} />
+    </StaticRouter>
+  );
+
+  const petThumbnail = await screen.findByTestId("thumbnail");
+
+  expect(petThumbnail.src).toContain("1.jpg");
 
   screen.debug();
 });
