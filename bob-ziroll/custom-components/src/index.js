87f2e3f02e5d166c604559d62848071820571717
Paul Tobin <tobster111@gmail.com>
2022-01-14 20:46:21 +0000

    Custom 2

--- bob-ziroll/custom-components/src/index.js
+++ bob-ziroll/custom-components/src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const page = (
-  <div>
-    <img src="./react-logo.png" width="40px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function Page() {
+  return (
+    <ol>
+      <li>
+        It's a popular library, so I'll be able to fit in with the cool kids!
+      </li>
+      <li>I'm more likely to get a job as a developer if I know React</li>
+    </ol>
+  );
+}
 
-ReactDOM.render(page, document.getElementById('root'));
+ReactDOM.render(<Page />, document.getElementById('root'));
