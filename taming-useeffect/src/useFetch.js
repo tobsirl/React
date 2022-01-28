567b5eaeb7f327663cb9a6a673cc90dd92fdbef8
Paul Tobin <tobster111@gmail.com>
2022-01-28 18:47:20 +0000

    useEffect

--- taming-useeffect/src/useFetch.js
+++ taming-useeffect/src/useFetch.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const useFetch = (options) => {
   const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log('useFetch useEffect');
+    fetch(options.url)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
 
   return {
     data,
