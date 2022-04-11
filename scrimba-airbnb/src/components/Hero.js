c1ebe006a8bd9d42e99b00eefe0fc4dec310f823
Paul Tobin <tobster111@gmail.com>
2022-04-11 15:35:59 +0100

    Styling hero

--- scrimba-airbnb/src/components/Hero.js
+++ scrimba-airbnb/src/components/Hero.js
@@ -1,16 +1,14 @@
+import photoGrid from '../images/photo-grid.png';
+
 export default function Hero() {
   return (
-    <main>
-      <img
-        src="../images/photo-grid.png"
-        alt="airbnb experiences"
-        className="photo-grid"
-      />
+    <section className='hero'>
+      <img src={photoGrid} alt="airbnb experiences" className="photo-grid" />
       <h1 className="hero--title">Online Experiences</h1>
       <p className="hero--text">
         Join unique interactive activities led by one-of-a-kind hosts—all
         without leaving home.
       </p>
-    </main>
+    </section>
   );
 }
