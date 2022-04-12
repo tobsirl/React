2ccf1935d208f9f736e49e97c0e103b7279f17c7
Paul Tobin <tobster111@gmail.com>
2022-04-12 11:41:38 +0100

    Added image star

--- scrimba-airbnb/src/components/Card.js
+++ scrimba-airbnb/src/components/Card.js
@@ -1,7 +1,18 @@
-
+import cardImage from '../images/katie-zaferes.png';
+import star from '../images/star.png';
 
 export default function Card() {
   return (
-    <div>Card</div>
-  )
+    <div className="card">
+      <div class="card--image">
+        <img src={cardImage} alt="katie-zaferes" />
+      </div>
+      <div class="card--body">
+        <div class="card--body-img">
+          <img src={star} alt="star" />
+          <p class="card--body-rating">5.0</p>
+        </div>
+      </div>
+    </div>
+  );
 }
