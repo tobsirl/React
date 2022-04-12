3beb6d1d0a8493956aaebb756665ccdd6715f6ad
Paul Tobin <tobster111@gmail.com>
2022-04-12 11:53:38 +0100

    Styling the card

--- scrimba-airbnb/src/components/Card.js
+++ scrimba-airbnb/src/components/Card.js
@@ -6,13 +6,15 @@ export default function Card() {
     <div className="card">
       <img src={cardImage} alt="katie-zaferes" className="card--image" />
       <div class="card--stats">
-        <img src={star} alt="star" />
+        <img src={star} alt="star" className="card--star" />
         <span>5.0</span>
-        <span>(6) • </span>
-        <span>USA</span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
       </div>
       <p>Life Lessons with Katie Zaferes</p>
-      <p>From $136 / person</p>
+      <p>
+        <span className='bold'>From $136</span> / person
+      </p>
     </div>
   );
 }
