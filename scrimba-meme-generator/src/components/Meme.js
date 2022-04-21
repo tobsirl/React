e89b1e7b52f0444148d9f5179207a7d8ea8f9fef
Paul Tobin <tobster111@gmail.com>
2022-04-21 19:30:30 +0100

    Placeholder

--- scrimba-meme-generator/src/components/Meme.js
+++ scrimba-meme-generator/src/components/Meme.js
@@ -2,8 +2,8 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <input type="text" className="form--input"/>
-        <input type="text" className="form--input"/>
+        <input type="text" className="form--input" placeholder="Top text" />
+        <input type="text" className="form--input" placeholder="Bottom text" />
         <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
