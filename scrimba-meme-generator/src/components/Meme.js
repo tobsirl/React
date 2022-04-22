616b719b011e034ec75c31a2e8707cd53e074145
Paul Tobin <tobster111@gmail.com>
2022-04-22 18:28:55 +0100

    Change form to div

--- scrimba-meme-generator/src/components/Meme.js
+++ scrimba-meme-generator/src/components/Meme.js
@@ -1,11 +1,11 @@
 export default function Meme() {
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form--input" placeholder="Top text" />
         <input type="text" className="form--input" placeholder="Bottom text" />
         <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      </div>
     </main>
   );
 }
