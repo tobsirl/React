34d3afb3b5ed8d00ce422406e88ed081fad5f9da
Paul Tobin <tobster111@gmail.com>
2022-04-26 18:51:38 +0100

    Finished

--- scrimba-boxes/src/App.js
+++ scrimba-boxes/src/App.js
@@ -3,22 +3,12 @@ import boxes from './boxes';
 import './style.css';
 
 export default function App() {
-  /**
-   * Challenge part 1:
-   * 1. Initialize state with the default value of the
-   *    array pulled in from boxes.js
-   * 2. Map over that state array and display each one
-   *    as an empty square (black border, transparent bg color)
-   *    (Don't worry about using the "on" property yet)
-   */
-  const [boxArr, setBoxArr] = React.useState(boxes);
+  const [squares, setSquares] = React.useState(boxes);
   return (
     <main>
-      <h1>Boxes will go here</h1>
       <div className="boxes">
-        {' '}
-        {boxArr.map((b) => (
-          <p className="box">{b.id}</p>
+        {squares.map((box) => (
+          <p className="box" key={box.id}></p>
         ))}
       </div>
     </main>
