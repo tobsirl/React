fc5a096acb027834a23b1eaa0c247337a9427b4e
Paul Tobin <tobster111@gmail.com>
2022-04-27 18:21:33 +0100

    Chanllenge complet

--- scrimba-boxes/src/App.js
+++ scrimba-boxes/src/App.js
@@ -3,19 +3,8 @@ import Box from './Box';
 import boxes from './boxes';
 import './style.css';
 
-export default function App(props) {
+export default function App() {
   const [squares, setSquares] = React.useState(boxes);
-
-  /**
-   * Challenge part 2:
-   * 1. Create a separate component called "Box" and
-   *    replace the `div` above with our <Box /> components
-   * 2. Pass the Box component a prop called `on` with the
-   *    value of the same name from the `boxes` objects
-   * 3. In the Box component, apply dynamic styles to determine
-   *    the backgroundColor of the box. If it's `on`, set the
-   *    backgroundColor to "#222222". If off, set it to "none"
-   */
 
   return (
     <main>
--- scrimba-boxes/src/Box.js
+++ scrimba-boxes/src/Box.js
@@ -1,6 +1,6 @@
 export default function Box({ on }) {
   const styles = {
-    backgroundColor: on ? '#222222' : '#cccccc',
+    backgroundColor: on ? '#222222' : 'none',
   };
 
   return <div className="box" style={styles}></div>;
