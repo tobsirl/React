6dcdc9b125bd3d2b524d0d02bfc2c980b09762b2
Paul Tobin <tobster111@gmail.com>
2022-04-27 18:24:26 +0100

    Chanllenge 3

--- scrimba-boxes/src/Box.js
+++ scrimba-boxes/src/Box.js
@@ -1,4 +1,14 @@
 export default function Box({ on }) {
+  /**
+   * Challenge: Create state controlling whether
+   * this box is "on" or "off". Use the incoming
+   * `props.on` to determine the initial state.
+   *
+   * Create an event listener so when the box is clicked,
+   * it toggles from "on" to "off".
+   *
+   * Goal: clicking each box should toggle it on and off.
+   */
   const styles = {
     backgroundColor: on ? '#222222' : 'none',
   };
