51ce8d496c27dbe683def38a07b5ee63f04e55ea
Paul Tobin <tobster111@gmail.com>
2022-04-27 18:32:19 +0100

    Chanllenge cleanup

--- scrimba-boxes/src/Box.js
+++ scrimba-boxes/src/Box.js
@@ -1,17 +1,6 @@
 import React from 'react';
 
 export default function Box({ on }) {
-  /**
-   * Challenge: Create state controlling whether
-   * this box is "on" or "off". Use the incoming
-   * `props.on` to determine the initial state.
-   *
-   * Create an event listener so when the box is clicked,
-   * it toggles from "on" to "off".
-   *
-   * Goal: clicking each box should toggle it on and off.
-   */
-
   const [state, setState] = React.useState(on);
 
   function handleClick() {
