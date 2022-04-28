b954bf3961b00527efeb227eb3796822083f3521
Paul Tobin <tobster111@gmail.com>
2022-04-28 19:48:49 +0100

    Bad way

--- scrimba-boxes/src/App.js
+++ scrimba-boxes/src/App.js
@@ -5,14 +5,24 @@ import './style.css';
 
 export default function App() {
   const [squares, setSquares] = React.useState(boxes);
-  console.log(squares);
 
   function handleClick(id) {
-    // setSquares((prevState) => ({
-    //   ...prevState,
-    //   prevState: !prevState.on,
-    // }));
-    console.log('Clicked', id);
+    setSquares((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
   }
 
   /**
