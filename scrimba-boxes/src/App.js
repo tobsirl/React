d7d5584b6025cce0f4af1915f857b386a83b4845
Paul Tobin <tobster111@gmail.com>
2022-04-28 19:55:37 +0100

    Better way

--- scrimba-boxes/src/App.js
+++ scrimba-boxes/src/App.js
@@ -8,20 +8,9 @@ export default function App() {
 
   function handleClick(id) {
     setSquares((prevSquares) => {
-      const newSquares = [];
-      for (let i = 0; i < prevSquares.length; i++) {
-        const currentSquare = prevSquares[i];
-        if (currentSquare.id === id) {
-          const updatedSquare = {
-            ...currentSquare,
-            on: !currentSquare.on,
-          };
-          newSquares.push(updatedSquare);
-        } else {
-          newSquares.push(currentSquare);
-        }
-      }
-      return newSquares;
+      return prevSquares.map((square) => {
+        return square.id === id ? { ...square, on: !square.on } : square;
+      });
     });
   }
 
