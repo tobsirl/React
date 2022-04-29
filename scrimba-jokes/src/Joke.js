c16f296224359d4b9c11a0bfbc00ad7dc0be341b
Paul Tobin <tobster111@gmail.com>
2022-04-29 17:41:57 +0100

    Challenge complete

--- scrimba-jokes/src/Joke.js
+++ scrimba-jokes/src/Joke.js
@@ -7,10 +7,15 @@ export default function Joke(props) {
    * - Create state `isShown` (boolean, default to `false`)
    * - Add a button that toggles the value back and forth
    */
+
+  function toggleShown() {
+    setIsShown((prevState) => !prevState);
+  }
   return (
     <div>
       {props.setup && <h3>{props.setup}</h3>}
-      <p>{props.punchline}</p>
+      {isShown && <p>{props.punchline}</p>}
+      <button onClick={toggleShown}>Show Punchline</button>
       <hr />
     </div>
   );
