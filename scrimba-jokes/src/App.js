b39677fbc48e4542a631308d9e4e2487670b92d3
Paul Tobin <tobster111@gmail.com>
2022-04-30 12:26:20 +0100

    container

--- scrimba-jokes/src/App.js
+++ scrimba-jokes/src/App.js
@@ -5,7 +5,11 @@ import './style.css'
 
 export default function App() {
   const jokeElements = jokesData.map((joke) => {
-    return <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />
+    return (
+      <div className="container">
+        <Joke key={joke.id} setup={joke.setup} punchline={joke.punchline} />
+      </div>
+    )
   })
 
   return <div>{jokeElements}</div>
