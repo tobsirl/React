eeaefb7ce62cef3d1c803ca2be2aa2674157537d
Paul Tobin <tobster111@gmail.com>
2022-05-01 10:48:50 +0100

    forms styling

--- scrimba-forms/src/App.js
+++ scrimba-forms/src/App.js
@@ -1,7 +1,48 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">test</div>;
+  const [formData, setFormData] = React.useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+      <textarea></textarea>
+    </form>
+  );
 }
 
 export default App;
