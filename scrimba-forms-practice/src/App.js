5832d5defbc64f98222e340c7b196fe0c8fb85d1
Paul Tobin <tobster111@gmail.com>
2022-05-04 18:13:25 +0100

    Cleanup

--- scrimba-forms-practice/src/App.js
+++ scrimba-forms-practice/src/App.js
@@ -16,21 +16,10 @@ export default function App() {
       [name]: type === 'checkbox' ? checked : value,
     }));
   }
-  /**
-   * Challenge: Connect the form to local state
-   *
-   * 3. When the user clicks "Sign up", check if the
-   *    password & confirmation match each other. If
-   *    so, log "Successfully signed up" to the console.
-   *    If not, log "passwords to not match" to the console.
-   * 4. Also when submitting the form, if the person checked
-   *    the "newsletter" checkbox, log "Thanks for signing
-   *    up for our newsletter!" to the console.
-   */
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+
     const passwordCheck = formData.password === formData.passwordConfirm;
 
     const check = passwordCheck
