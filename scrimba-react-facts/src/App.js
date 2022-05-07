14a401612222531c42dcd528178e4ecb65362fea
Paul Tobin <tobster111@gmail.com>
2022-05-07 10:42:28 +0100

    State in app

--- scrimba-react-facts/src/App.js
+++ scrimba-react-facts/src/App.js
@@ -1,12 +1,18 @@
+import React from 'react'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 import './style.css'
 
 export default function App() {
+  const [darkMode, setDarkMode] = React.useState('')
+
+  function toggleDarkMode() {
+    setDarkMode(darkMode === 'dark' ? '' : 'dark')
+  }
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   )
 }
