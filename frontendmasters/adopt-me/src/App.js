966702537bcf22bab19f94534e1a0e852afceda1
Paul Tobin <tobster111@gmail.com>
2022-12-14 16:16:16 +0000

    React v8 props animals

--- frontendmasters/adopt-me/src/App.js
+++ frontendmasters/adopt-me/src/App.js
@@ -9,9 +9,21 @@ function Pet(props) {
 function App() {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Doink',
+      animal: 'Cat',
+      breed: 'Mix',
+    }),
   ]);
 }
 
