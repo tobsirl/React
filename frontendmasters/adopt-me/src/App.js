1cf0e3851d3067fefb4d25ebfe7c7876a1b7501e
Paul Tobin <tobster111@gmail.com>
2022-12-14 15:50:54 +0000

    React v8 pure components

--- frontendmasters/adopt-me/src/App.js
+++ frontendmasters/adopt-me/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+function Pet() {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Luna'),
+    React.createElement('h2', {}, 'Dog'),
+    React.createElement('h2', {}, 'Havanese'),
+  ]);
+}
+
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet),
   ]);
 };
 
