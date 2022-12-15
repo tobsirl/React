f4366b2b4f9f551afef802b12356f29117c5df72
Paul Tobin <tobster111@gmail.com>
2022-12-15 10:18:50 +0000

    React v8 formatting

--- frontendmasters/adopt-me/src/App.js
+++ frontendmasters/adopt-me/src/App.js
@@ -1,32 +1,32 @@
 function Pet(props) {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 }
 
 function App() {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: 'Luna',
-      animal: 'Dog',
-      breed: 'Havanese',
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
-      name: 'Pepper',
-      animal: 'Bird',
-      breed: 'Cockatiel',
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
     }),
     React.createElement(Pet, {
-      name: 'Doink',
-      animal: 'Cat',
-      breed: 'Mix',
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mix",
     }),
   ]);
 }
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
