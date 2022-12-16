0f7d434cbaf9755774c5636cd45b1afa98c6e5f4
Paul Tobin <tobster111@gmail.com>
2022-12-16 12:43:07 +0000

    React v8 Pet component

--- frontendmasters/adopt-me/src/Pet.jsx
+++ frontendmasters/adopt-me/src/Pet.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-export default function Pet(props) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
+// export default function Pet(props) {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+// }
+
+export default function Pet({ name, animal, breed }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
