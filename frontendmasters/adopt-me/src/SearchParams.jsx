d0ae8739d2c35fb283481c58650fba04afb1199b
Paul Tobin <tobster111@gmail.com>
2022-12-17 10:42:09 +0000

    React v8 SearchParams useState

--- frontendmasters/adopt-me/src/SearchParams.jsx
+++ frontendmasters/adopt-me/src/SearchParams.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function SearchParams() {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={() => setLocation(event.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
