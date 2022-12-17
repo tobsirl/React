4fe23e556cec30c32633be4352953eb925ad9971
Paul Tobin <tobster111@gmail.com>
2022-12-17 10:29:45 +0000

    React v8 SearchParams

---
+++ frontendmasters/adopt-me/src/SearchParams.jsx
@@ -0,0 +1,14 @@
+export default function SearchParams() {
+  const location = "Seattle, WA";
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input id="location" value={location} placeholder="Location" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
