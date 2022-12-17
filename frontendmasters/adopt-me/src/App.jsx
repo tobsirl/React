41a40acf41d379062bd16127d1560cd21d39e691
Paul Tobin <tobster111@gmail.com>
2022-12-17 10:56:30 +0000

    React v8 SearchParams preitter ignore

--- frontendmasters/adopt-me/src/App.jsx
+++ frontendmasters/adopt-me/src/App.jsx
@@ -8,9 +8,6 @@ function App() {
     <div>
       <h1 id="something-important">Adopt Me!</h1>
       <SearchParams />
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
     </div>
   )
 }
