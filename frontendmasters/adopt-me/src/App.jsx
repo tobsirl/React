19a38b1fe06b55502236ff5fff4bbfc379c09fcd
Paul Tobin <tobster111@gmail.com>
2022-12-17 10:34:09 +0000

    React v8 SearchParams

--- frontendmasters/adopt-me/src/App.jsx
+++ frontendmasters/adopt-me/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 function App() {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
       <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
       <Pet name="Doink" animal="Cat" breed="Mixed" />
