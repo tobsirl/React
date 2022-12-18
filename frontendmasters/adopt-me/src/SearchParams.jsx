ad47525653ef830036c1910c7aa09c4d6fd70fe9
Paul Tobin <tobster111@gmail.com>
2022-12-18 10:58:01 +0000

    React v8 useBreedList

--- frontendmasters/adopt-me/src/SearchParams.jsx
+++ frontendmasters/adopt-me/src/SearchParams.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react'
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
 import Pet from './Pet'
+import useBreedList from './useBreedList'
 
 export default function SearchParams() {
   const [location, setLocation] = useState('')
   const [animal, updateAnimal] = useState('')
   const [breed, setBreed] = useState('')
   const [pets, setPets] = useState([])
-  const breeds = []
+  const [breeds] = useBreedList(animal)
 
   useEffect(() => {
     requestPets()
