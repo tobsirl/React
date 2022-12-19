237787c94dd49a028bddffe456ebc22df28de2d5
Paul Tobin <tobster111@gmail.com>
2022-12-19 10:24:22 +0000

    React v8 React QueryClientProvider

--- frontendmasters/adopt-me/src/App.jsx
+++ frontendmasters/adopt-me/src/App.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Details from './Details'
 import SearchParams from './SearchParams'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+})
 
 function App() {
   return (
     <Router>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </Router>
   )
 }
