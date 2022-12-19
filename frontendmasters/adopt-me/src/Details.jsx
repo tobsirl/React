86361600302afbfb0458874b88963fc6cb2bee6e
Paul Tobin <tobster111@gmail.com>
2022-12-19 11:29:38 +0000

    React v8 Details with pet info

--- frontendmasters/adopt-me/src/Details.jsx
+++ frontendmasters/adopt-me/src/Details.jsx
@@ -1,6 +1,19 @@
 import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import fetchPet from './fetchPet'
 
 export default function Details() {
   const { id } = useParams()
-  return <h2>{id}</h2>
+  const results = useQuery(['details', id], fetchPet)
+  if (results.isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (results.isError) {
+    return <h2>Error!</h2>
+  }
+
+  const pet = results.data.pet[0]
+
+  return <h2>{pet}</h2>
 }
