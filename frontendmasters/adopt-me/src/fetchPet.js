29cbf5c37b9e97a1a2110352651af7dedad873f6
Paul Tobin <tobster111@gmail.com>
2022-12-19 10:32:52 +0000

    React v8 fetchPet

---
+++ frontendmasters/adopt-me/src/fetchPet.js
@@ -0,0 +1,13 @@
+async function fetchPet({ queryKey }) {
+  const id = queryKey[1]
+
+  const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`)
+
+  if (!apiRes.ok) {
+    throw new Error(`Error fetching pet with id: ${id}`)
+  }
+
+  return apiRes.json()
+}
+
+export default fetchPet
