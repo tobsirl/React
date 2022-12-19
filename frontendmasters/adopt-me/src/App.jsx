6c98177ec44bfbcac564690aa7f13f52699df842
Paul Tobin <tobster111@gmail.com>
2022-12-19 10:04:55 +0000

    React v8 BrowserRouter

--- frontendmasters/adopt-me/src/App.jsx
+++ frontendmasters/adopt-me/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Details from './Details'
 import SearchParams from './SearchParams'
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <h1 id="something-important">Adopt Me!</h1>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
