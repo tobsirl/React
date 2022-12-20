4ae7dbd70aa23c4b6cbef4e26a60f06d814f8789
Paul Tobin <tobster111@gmail.com>
2022-12-20 12:36:32 +0000

    React v8 Carousel in Details

--- frontendmasters/adopt-me/src/Details.jsx
+++ frontendmasters/adopt-me/src/Details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import fetchPet from './fetchPet'
+import Carousel from './Carousel'
 
 export default function Details() {
   const { id } = useParams()
@@ -18,6 +19,7 @@ export default function Details() {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} - ${pet.breed} - ${pet.location}`}</h2>
