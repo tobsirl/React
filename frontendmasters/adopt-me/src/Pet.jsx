c943b27f14ffdca5f19d682e5bc1aad5edd4e5b6
Paul Tobin <tobster111@gmail.com>
2022-12-23 11:21:55 +0000

    React v8 tailwind labels

--- frontendmasters/adopt-me/src/Pet.jsx
+++ frontendmasters/adopt-me/src/Pet.jsx
@@ -7,11 +7,11 @@ export default function Pet({ name, animal, breed, images, location, id }) {
   }
   return (
     <div className="pet">
-      <Link to={`/details/${id}`} className="pet">
+      <Link to={`/details/${id}`} className="relative block">
         <div className="image-container">
           <img src={hero} alt={name} />
         </div>
-        <div className="info">
+        <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
         </div>
