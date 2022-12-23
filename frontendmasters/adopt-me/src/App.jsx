bd0209f5aa1c068b0414463d24a292e659ae625d
Paul Tobin <tobster111@gmail.com>
2022-12-23 10:43:57 +0000

    React v8 styling header

--- frontendmasters/adopt-me/src/App.jsx
+++ frontendmasters/adopt-me/src/App.jsx
@@ -28,8 +28,10 @@ function App() {
       <Router>
         <QueryClientProvider client={queryClient}>
           <AdoptedPetContext.Provider value={adoptedPet}>
-            <header className="mb-10 w-full bg-gradient-to-b p-7 text-center">
-              <Link to="/">Adopt Me!</Link>
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
             </header>
             <Routes>
               <Route path="/details/:id" element={<Details />} />
