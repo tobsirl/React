d5e623a89558a0f881b7141d48f0d02f608d6407
Paul Tobin <tobster111@gmail.com>
2022-12-25 11:09:06 +0000

    React v8 Typescript format linting

--- frontendmasters/adopt-me-ts/src/Modal.tsx
+++ frontendmasters/adopt-me-ts/src/Modal.tsx
@@ -1,7 +1,7 @@
-import  { useEffect, useRef, MutableRefObject, ReactElement } from "react";
+import { useEffect, useRef, MutableRefObject, ReactElement } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }: {children: ReactElement}) => {
+const Modal = ({ children }: { children: ReactElement }) => {
   const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
@@ -13,11 +13,11 @@ const Modal = ({ children }: {children: ReactElement}) => {
       return;
     }
     modalRoot.appendChild(elRef.current);
-    return () =>{
+    return () => {
       if (elRef.current) {
         modalRoot.removeChild(elRef.current);
       }
-    }
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
