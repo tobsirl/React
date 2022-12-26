288f5cae353c83d1349a3c34ef17e8800d0a1ee7
Paul Tobin <tobster111@gmail.com>
2022-12-26 10:29:27 +0000

    React v8 AdoptedPetContext

---
+++ frontendmasters/adopt-me-ts/src/AdoptedPetContext.ts
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { Pet } from "./APIResponsesTypes";
+
+const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void]>([
+  {
+    id: 0,
+    name: "Fido",
+    animal: "dog",
+    description: "description",
+    breed: "Beagle",
+    images: [],
+    city: "Seattle",
+    state: "WA",
+  },
+  () => {},
+]);
+
+export default AdoptedPetContext;
