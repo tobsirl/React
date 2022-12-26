266887d033027e9a145750380e13461de609640e
Paul Tobin <tobster111@gmail.com>
2022-12-26 10:15:08 +0000

    React v8 APIResponseTypes interface Pet

--- frontendmasters/adopt-me-ts/src/APIResponsesTypes.ts
+++ frontendmasters/adopt-me-ts/src/APIResponsesTypes.ts
@@ -1,1 +1,12 @@
 export type Animal = "dog" | "cat" | "bird" | "reptile" | "rabbit";
+
+export interface Pet {
+  id: number;
+  name: string;
+  animal: Animal;
+  description: string;
+  breed: string;
+  images: string[];
+  city: string;
+  state: string;
+}
