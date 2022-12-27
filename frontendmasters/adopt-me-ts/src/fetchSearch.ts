480cab2228ba06f2449ad791b68681e58b059911
Paul Tobin <tobster111@gmail.com>
2022-12-27 09:43:59 +0000

    React v8 ts FetchSearch

--- frontendmasters/adopt-me-ts/src/fetchSearch.ts
+++ frontendmasters/adopt-me-ts/src/fetchSearch.ts
@@ -1,4 +1,17 @@
-async function fetchSearch({ queryKey }) {
+import { QueryFunction } from "@tanstack/react-query";
+import { PetAPIResponse } from "./APIResponsesTypes";
+
+const fetchSearch: QueryFunction<
+  PetAPIResponse,
+  [
+    "search",
+    {
+      location: string;
+      animal: string;
+      breed: string;
+    }
+  ]
+> = async ({ queryKey }) => {
   const { animal, location, breed } = queryKey[1];
   const res = await fetch(
     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
@@ -8,6 +21,6 @@ async function fetchSearch({ queryKey }) {
     throw new Error(`pet search not okay: ${animal}, ${location}, ${breed}`);
 
   return res.json();
-}
+};
 
 export default fetchSearch;
