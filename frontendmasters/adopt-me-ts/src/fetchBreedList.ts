d7832157cfb6260ee40e7e41d5bc6fb2115a63b8
Paul Tobin <tobster111@gmail.com>
2022-12-27 09:58:13 +0000

    React v8 ts FetchBreedList

--- frontendmasters/adopt-me-ts/src/fetchBreedList.ts
+++ frontendmasters/adopt-me-ts/src/fetchBreedList.ts
@@ -7,8 +7,6 @@ const fetchBreedList: QueryFunction<
 > = async ({ queryKey }) => {
   const animal = queryKey[1];
 
-  if (!animal) return [];
-
   const res = await fetch(
     `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
   );
