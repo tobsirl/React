c92de617b1f80113c7d0d37714a0f0157ee1be2b
Paul Tobin <tobster111@gmail.com>
2022-12-28 10:13:51 +0000

    React v8 Redux adoptedPet in the store

--- frontendmasters/adopt-me-redux/src/store.js
+++ frontendmasters/adopt-me-redux/src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import adoptedPet from "./adoptedPetSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    adoptedPet,
+  },
 });
 
 export default store;
