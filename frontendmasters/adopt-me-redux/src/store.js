1944a243d63d6eb99e84e23b94b66b3aae8ddb34
Paul Tobin <tobster111@gmail.com>
2022-12-29 10:04:35 +0000

    React v8 PetApiService to store

--- frontendmasters/adopt-me-redux/src/store.js
+++ frontendmasters/adopt-me-redux/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchParams from "./searchParamsSlice";
 import adoptedPet from "./adoptedPetSlice";
+import { petApi } from "./PetApiService";
 
 const store = configureStore({
   reducer: {
     adoptedPet,
     searchParams,
+    [petApi.reducerPath]: petApi.reducer,
   },
 });
 
