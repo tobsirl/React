f4dec3cbc1116290eb6c4783497ade407919de8e
Paul Tobin <tobster111@gmail.com>
2022-12-30 10:34:11 +0000

    React v8 Testing Pet StaticRouter

--- frontendmasters/adopt-me-testing/src/__test__/Pet.test.jsx
+++ frontendmasters/adopt-me-testing/src/__test__/Pet.test.jsx
@@ -1,9 +1,14 @@
 import { expect, test } from "vitest";
 import { render } from "@testing-library/react";
+import { StaticRouter } from "react-router-dom/server";
 import Pet from "../Pet";
 
 test("displays a default thumbnail", async () => {
-  const pet = render(<Pet />);
+  const pet = render(
+    <StaticRouter>
+      <Pet />
+    </StaticRouter>
+  );
 
   const petThumbnail = await pet.findByTestId("thumbnail");
   expect(petThumbnail.src).toContain("none.jpg");
