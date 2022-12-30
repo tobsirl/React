dcacb4abf211c4a2fb7a8f0682b764afbd8bb5ab
Paul Tobin <tobster111@gmail.com>
2022-12-30 10:41:29 +0000

    React v8 Testing Carousel tests

--- frontendmasters/adopt-me-testing/src/Carousel.jsx
+++ frontendmasters/adopt-me-testing/src/Carousel.jsx
@@ -20,7 +20,7 @@ class Carousel extends Component {
     const { images } = this.props;
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal" />
+        <img src={images[active]} alt="animal" data-testid="hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
             // eslint-disable-next-line
@@ -31,6 +31,7 @@ class Carousel extends Component {
               alt="animal thumbnail"
               onClick={this.handleIndexClick}
               data-index={index}
+              data-testid={`thumbnail${index}`}
             />
           ))}
         </div>
