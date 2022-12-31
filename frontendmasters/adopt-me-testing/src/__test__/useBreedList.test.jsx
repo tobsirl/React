88df7317d70b776866a0785c7e1b06102f35feb2
Paul Tobin <tobster111@gmail.com>
2022-12-31 09:40:42 +0000

    React v8 Mocking

---
+++ frontendmasters/adopt-me-testing/src/__test__/useBreedList.test.jsx
@@ -0,0 +1,27 @@
+import { expect, test } from "vitest";
+import { renderHook } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import useBreedList from "../useBreedList";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      retry: false,
+    },
+  },
+});
+
+test("gives an empty list with no animal", async () => {
+  const { result } = renderHook(() => useBreedList(""), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+
+  const [breedList, status] = result.current;
+
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("loading");
+});
