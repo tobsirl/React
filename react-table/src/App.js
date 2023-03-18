6b3c895cd072b63e9ee7ede1347fe7d4223a5177
Paul Tobin <tobster111@gmail.com>
2023-03-18 10:21:49 +0000

    React Table setup react query

--- react-table/src/App.js
+++ react-table/src/App.js
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import './App.css';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-6xl font-bold underline">tailwindcss</h1>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-6xl font-bold underline">React Table</h1>
+        </header>
+      </div>
+    </QueryClientProvider>
   );
 }
 
