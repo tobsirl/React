0b679f56e031e7404ae7299a32c37c4445932158
Paul Tobin <tobster111@gmail.com>
2023-03-18 10:19:30 +0000

    React Table install tailwind and react-table

--- react-table/src/App.js
+++ react-table/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,18 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="text-6xl font-bold underline">tailwindcss</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ react-table/src/components/Table.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+export default function Table() {
+  const [products, setProducts] = useState([]);
+
+  
+  return <div>Table</div>;
+}
