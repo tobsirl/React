deb87b9d8f4556522bda4789186393c956030f90
Paul Tobin <tobster111@gmail.com>
2023-04-06 09:34:47 +0100

    React Typescript State

--- frontendmasters/react-typescript/accident-counter/tailwind.config.js
+++ frontendmasters/react-typescript/accident-counter/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: { ...colors, primary: colors.red },
+    },
   },
   plugins: [],
-}
-
+};
