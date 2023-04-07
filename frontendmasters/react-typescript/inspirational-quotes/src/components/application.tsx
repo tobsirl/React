39250788828fc92bb4bd1a05bcf551fb24e8aa68
Paul Tobin <tobster111@gmail.com>
2023-04-07 10:20:59 +0100

    React Typescript API

--- frontendmasters/react-typescript/inspirational-quotes/src/components/application.tsx
+++ frontendmasters/react-typescript/inspirational-quotes/src/components/application.tsx
@@ -20,7 +20,7 @@ export const fetchQuotes = async (count: number) => {
 };
 
 const Application = () => {
-  const [quote, setQuote] = useState();
+  const [quote, setQuote] = useState({content: '', source: ''});
 
   useEffect(() => {
     fetchRandomQuote().then(setQuote);
