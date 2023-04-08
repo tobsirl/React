698723f7da81c741b0bb7e323d84927b5af4ee03
Paul Tobin <tobster111@gmail.com>
2023-04-08 09:44:44 +0100

    React Typescript

--- frontendmasters/react-typescript/accident-counter/src/components/counter.tsx
+++ frontendmasters/react-typescript/accident-counter/src/components/counter.tsx
@@ -41,23 +41,23 @@ const reducer = (state = initialState, action: any) => {
 };
 
 const Counter = () => {
-  const [count, setCount] = useReducer(reducer, 0);
-  const [draftCount, setDraftCount] = useState(count);
+  const [state, dispatch] = useReducer(reducer, initialState);
+ 
 
   return (
     <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">{count}</p>
+      <p className="text-6xl">{state.count}</p>
       <div className="flex gap-2">
-        <button onClick={() => setCount(count - 1)}>➖ Decrement</button>
-        <button onClick={() => setCount(0)}>🔁 Reset</button>
-        <button onClick={() => setCount(count + 1)}>➕ Increment</button>
+        <button onClick={() => dispatch({type: 'decrement'})}>➖ Decrement</button>
+        <button onClick={() => dispatch({type: 'reset'})}>🔁 Reset</button>
+        <button onClick={() => dispatch({type: 'increment'})}>➕ Increment</button>
       </div>
       <div>
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            setCount(draftCount);
+            dispatch({type: 'updateCountFromDraft', payload: });
           }}
         >
           <input
