4329a058a8ace0e3d17e9fefea50f1202c0b8fb7
Paul Tobin <tobster111@gmail.com>
2023-04-08 10:09:37 +0100

    React Typescript

--- frontendmasters/react-typescript/accident-counter/src/components/counter.tsx
+++ frontendmasters/react-typescript/accident-counter/src/components/counter.tsx
@@ -42,28 +42,33 @@ const reducer = (state = initialState, action: any) => {
 
 const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
- 
 
   return (
     <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
       <p className="text-6xl">{state.count}</p>
       <div className="flex gap-2">
-        <button onClick={() => dispatch({type: 'decrement'})}>➖ Decrement</button>
-        <button onClick={() => dispatch({type: 'reset'})}>🔁 Reset</button>
-        <button onClick={() => dispatch({type: 'increment'})}>➕ Increment</button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>
+          ➖ Decrement
+        </button>
+        <button onClick={() => dispatch({ type: 'reset' })}>🔁 Reset</button>
+        <button onClick={() => dispatch({ type: 'increment' })}>
+          ➕ Increment
+        </button>
       </div>
       <div>
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            dispatch({type: 'updateCountFromDraft', payload: });
+            dispatch({ type: 'updateCountFromDraft' });
           }}
         >
           <input
             type="number"
             value={state.draftCount}
-            onChange={(event) => setDraftCount(event.target.valueAsNumber)}
+            onChange={(e) =>
+              dispatch({ action: 'updateDraftCount', payload: e.target.value })
+            }
           />
           <button type="submit">Update Counter</button>
         </form>
