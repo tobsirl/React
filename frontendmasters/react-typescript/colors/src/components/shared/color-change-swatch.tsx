5550403f3edcecca5da9db9cc1d19a96462c50d2
Paul Tobin <tobster111@gmail.com>
2023-04-11 18:36:48 +0100

    React Typescript context

--- frontendmasters/react-typescript/colors/src/components/shared/color-change-swatch.tsx
+++ frontendmasters/react-typescript/colors/src/components/shared/color-change-swatch.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, useContext } from 'react';
 import Button from './button';
+import { ColorContext } from '../../context';
 
 type ColorChangeSwatchProps = {
   hexColor: string;
@@ -13,6 +14,8 @@ const ColorChangeSwatch = ({
   className,
   onClick,
 }: ColorChangeSwatchProps) => {
+  const { dispatch } = useContext(ColorContext);
+
   return (
     <Button
       className={clsx(
@@ -20,7 +23,9 @@ const ColorChangeSwatch = ({
         className,
       )}
       style={{ backgroundColor: hexColor }}
-      onClick={onClick}
+      onClick={() =>
+        dispatch({ type: 'update-hex-color', payload: { hexColor } })
+      }
     >
       {hexColor}
     </Button>
