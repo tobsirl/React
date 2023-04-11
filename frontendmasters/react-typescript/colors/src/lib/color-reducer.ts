0eba8091e2eac27039118d87353d7fd2f7db9af7
Paul Tobin <tobster111@gmail.com>
2023-04-11 18:33:30 +0100

    React Typescript context

--- frontendmasters/react-typescript/colors/src/lib/color-reducer.ts
+++ frontendmasters/react-typescript/colors/src/lib/color-reducer.ts
@@ -1,21 +1,5 @@
 import { rgb } from 'color-convert';
 
-type UpdateHexColorAction = {
-  type: 'update-hex-color';
-  payload: {
-    hexColor: string;
-  };
-};
-
-type UpdateRGBColorAction = {
-  type: 'update-rgb-color';
-  payload: { rgb: [number, number, number] };
-};
-
-type ColorState = {
-  hexColor: string;
-};
-
 export const initialState: ColorState = {
   hexColor: '#BADA55',
 };
