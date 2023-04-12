8a703d668b6f0188d97c299135a91c0560f3cd5f
Paul Tobin <tobster111@gmail.com>
2023-04-12 19:00:58 +0100

    React Typescript packing list

--- frontendmasters/react-typescript/packing-list/src/components/application.tsx
+++ frontendmasters/react-typescript/packing-list/src/components/application.tsx
@@ -12,28 +12,6 @@ import MarkAllAsUnpacked from "./mark-all-as-unpacked";
 import NewItem from "./new-item";
 
 const Application = () => {
-  const [items, setItems] = useState(getInitialItems());
-
-  const add = (name: string) => {
-    const item = createItem(name);
-    setItems([...items, item]);
-  };
-
-  const update = (id: string, updates: any) => {
-    setItems(updateItem(items, id, updates));
-  };
-
-  const remove = (id: string) => {
-    setItems(removeItem(items, id));
-  };
-
-  const unpackedItems = filterItems(items, { packed: false });
-  const packedItems = filterItems(items, { packed: true });
-
-  const markAllAsUnpacked = () => {
-    return setItems(items.map((item) => ({ ...item, packed: false })));
-  };
-
   return (
     <main className="mx-auto flex flex-col gap-8 p-8 lg:max-w-4xl">
       <Header items={items} />
