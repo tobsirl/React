d39f9daf9e1bf1b2f1f30420cc00c4b6602b20cc
Paul Tobin <tobster111@gmail.com>
2023-06-17 10:45:02 +0100

    Ult React Pizza Menu

--- ult-react/03-pizza-menu/src/index.js
+++ ult-react/03-pizza-menu/src/index.js
@@ -49,16 +49,29 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Pizza Menu</h1>
+      <Header />
       <Pizza />
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
 }
 
 function Pizza() {
   return (
     <div>
-      <img src="pizzas/spinaci.jpg" alt="" />
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
