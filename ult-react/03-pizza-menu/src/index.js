f5ab7bf43e8df14473496e4f5ed2c1e3b6616f92
Paul Tobin <tobster111@gmail.com>
2023-06-18 09:42:59 +0100

    Ult React Pizza Menu

--- ult-react/03-pizza-menu/src/index.js
+++ ult-react/03-pizza-menu/src/index.js
@@ -76,6 +76,14 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  if (hour < openHour || hour > closeHour) {
+    return <footer>We're currently closed</footer>;
+  }
+
   return (
     <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
