a84b6d7f273720060f554a7a6208d767bdfd51ad
Paul Tobin <tobster111@gmail.com>
2023-06-23 17:20:06 +0100

    Ultra Steps

--- ult-react/steps/src/App.js
+++ ult-react/steps/src/App.js
@@ -8,6 +8,15 @@ const messages = [
 
 export default function App() {
   const step = 3;
+
+  function handlePrevious() {
+    alert('Previous');
+  }
+
+  function handleNext() {
+    alert('Next');
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,14 +26,20 @@ export default function App() {
       </div>
 
       <p className="message">
-        Step {step}: {messages[0]}
+        Step {step}: {messages[step - 1]}
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
