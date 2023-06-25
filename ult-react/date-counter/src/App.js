61a021458e2d86538e51a0780cb5a117991cfa53
Paul Tobin <tobster111@gmail.com>
2023-06-25 10:03:12 +0100

    Ultra date counter

--- ult-react/date-counter/src/App.js
+++ ult-react/date-counter/src/App.js
@@ -4,10 +4,17 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setstep] = useState(1);
   return (
-    <div className="counter">
-      <button onClick={() => setCount((c) => c - step)}>-</button>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c + step)}>+</button>
+    <div>
+      <div className="step">
+        <button onClick={() => setstep((s) => s - 1)}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={() => setstep((s) => s + 1)}>+</button>
+      </div>
+      <div className="counter">
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
     </div>
   );
 }
