c3c7dcc6df9e876cab437781747edaab69c7052b
Paul Tobin <tobster111@gmail.com>
2023-06-25 10:06:17 +0100

    Ultra date counter

--- ult-react/date-counter/src/App.js
+++ ult-react/date-counter/src/App.js
@@ -1,30 +1,45 @@
 import { useState } from 'react';
+import './styles.css';
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const [step, setstep] = useState(1);
+  const [step, setStep] = useState(1);
+
+  const date = new Date('june 21 2027');
+  date.setDate(date.getDate() + count);
+
   return (
     <div>
-      <div className="step">
-        <button onClick={() => setstep((s) => s - 1)}>-</button>
-        <p>Step: {step}</p>
-        <button onClick={() => setstep((s) => s + 1)}>+</button>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
-      <div className="counter">
+
+      <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <p>Count: {count}</p>
+        <span>Count: {count}</span>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Counter />
     </div>
   );
 }
-
-export default App;
