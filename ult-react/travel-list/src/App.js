05921facf64994ffb10f05c8707045783e004457
Paul Tobin <tobster111@gmail.com>
2023-06-26 18:44:46 +0100

    Ultra travel list

--- ult-react/travel-list/src/App.js
+++ ult-react/travel-list/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
 function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
@@ -13,7 +18,15 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Stats() {
@@ -21,6 +34,16 @@ function Stats() {
     <footer className="stats">
       <em>👜 You have X items on your list, and you already packed X (X%)</em>
     </footer>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      {item.quantity} {item.description}
+      <button className="remove">Remove</button>
+    </li>
   );
 }
 
