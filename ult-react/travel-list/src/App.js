759ca9ffb93faca6ef30781efbe370d8f23d3c87
Paul Tobin <tobster111@gmail.com>
2023-06-27 17:43:14 +0100

    travel list

--- ult-react/travel-list/src/App.js
+++ ult-react/travel-list/src/App.js
@@ -15,7 +15,9 @@ function Form() {
       <h3>What do you need for your 😍 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option>{num}</option>
+          <option value={num} key={num}>
+            {num}
+          </option>
         ))}
       </select>
       <input type="text" placeholder="Item..." />
@@ -28,7 +30,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
