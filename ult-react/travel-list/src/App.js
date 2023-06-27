c622176c436ad4967b0caa56051a4067111c909b
Paul Tobin <tobster111@gmail.com>
2023-06-27 17:44:04 +0100

    travel list

--- ult-react/travel-list/src/App.js
+++ ult-react/travel-list/src/App.js
@@ -10,8 +10,10 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit() {}
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
