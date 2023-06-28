36b555e8385e9637f9051cfb9305ccae8cc8c103
Paul Tobin <tobster111@gmail.com>
2023-06-28 17:42:07 +0100

    Travel list

--- ult-react/travel-list/src/App.js
+++ ult-react/travel-list/src/App.js
@@ -11,6 +11,7 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(5);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -19,7 +20,10 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
