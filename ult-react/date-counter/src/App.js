ae48136e259b9b6a892204ffbc4292a562e7ff48
Paul Tobin <tobster111@gmail.com>
2023-06-30 14:38:54 +0100

    React

--- ult-react/date-counter/src/App.js
+++ ult-react/date-counter/src/App.js
@@ -1,10 +1,22 @@
 import { useState } from 'react';
 import './styles.css';
 
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
 function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
-  const [range, setRange] = useState(0);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
 
   const date = new Date('june 21 2027');
   date.setDate(date.getDate() + count);
@@ -14,19 +26,21 @@ function Counter() {
       <div>
         <input
           type="range"
-          min={0}
-          max={10}
+          min="0"
+          max="10"
           value={step}
           onChange={(e) => setStep(Number(e.target.value))}
         />
-        <button onClick={() => setStep((c) => c - 1)}>-</button>
         <span>Step: {step}</span>
-        <button onClick={() => setStep((c) => c + 1)}>+</button>
       </div>
 
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
 
@@ -40,15 +54,12 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
-      {count > 0 ? <button onClick={() => setCount(0)}>Reset</button> : null}
-    </div>
-  );
-}
 
-export default function App() {
-  return (
-    <div className="App">
-      <Counter />
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
