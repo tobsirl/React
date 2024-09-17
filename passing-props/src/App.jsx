6afc50febbb05eb4ffbe77ae35602dafa9f88d59
Paul Tobin <tobster111@gmail.com>
2024-09-17 19:16:14 +0100

    changes

--- passing-props/src/App.jsx
+++ passing-props/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Greeting({ name }) {
+  const [index, setIndex] = React.useState(0)
+
+  const greetings = ['Hello', 'Hola', 'Bonjour']
+
+  const handleClick = () => {
+    const nextIndex = index === greetings.length - 1 ? 0 : index + 1
+    setIndex(nextIndex)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>
+        {greetings[index]}, {name}
+      </h1>
+      <button onClick={handleClick}>Next Greeting</button>
+    </main>
   )
 }
 
-export default App
+export default function App() {
+  return <Greeting name="Tyler" />
+}
