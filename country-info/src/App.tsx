9478d74420a5be30919762b792e7cfe38260c158
Paul Tobin <tobster111@gmail.com>
2025-05-28 21:45:13 +0100

    feat: refactor CountryInfo component and remove unused assets

--- country-info/src/App.tsx
+++ country-info/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function CountryInfo() {
+  const countryCode = "AU";
+  const data = null;
+  const isLoading = true;
+  const error = null;
 
+  const handleChange = (e) => {};
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <section>
+      <header>
+        <h1>Country Info:</h1>
 
-export default App
+        <label htmlFor="country">Select a country:</label>
+        <div>
+          <select id="country" value={countryCode} onChange={handleChange}>
+            <option value="AU">Australia</option>
+            <option value="CA">Canada</option>
+            <option value="CN">China</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+            <option value="IN">India</option>
+            <option value="JP">Japan</option>
+            <option value="MX">Mexico</option>
+            <option value="GB">United Kingdom</option>
+            <option value="US">United States of America</option>
+          </select>
+          {isLoading && <span>Loading...</span>}
+          {error && <span>{error.message}</span>}
+        </div>
+      </header>
+
+      {data && (
+        <article>
+          <h2>{data.name}</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>Capital:</td>
+                <td>{data.capital}</td>
+              </tr>
+              <tr>
+                <td>Region:</td>
+                <td>{data.region}</td>
+              </tr>
+              <tr>
+                <td>Population:</td>
+                <td>{data.population}</td>
+              </tr>
+              <tr>
+                <td>Area:</td>
+                <td>{data.area}</td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
+      )}
+    </section>
+  );
+}
