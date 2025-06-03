0c973c5483f500a00f4873f08fcca1f59422a715
Paul Tobin <tobster111@gmail.com>
2025-06-03 14:31:13 +0100

    feat: add autofocus functionality to email input field

--- auto-focus/src/App.jsx
+++ auto-focus/src/App.jsx
@@ -2,6 +2,12 @@ import { useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
   return (
     <>
       <h1>Autofocus input</h1>
@@ -10,6 +16,7 @@ function App() {
         type="email"
         id="focus"
         name="focus"
+        ref={inputRef}
         autoFocus
         placeholder="Enter your email"
       />
