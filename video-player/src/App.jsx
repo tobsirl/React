55978cb03d1b7b0e63b84b827224166870dee2f2
Paul Tobin <tobster111@gmail.com>
2025-06-04 10:00:00 +0100

    feat: add videoRef to video element for improved control

--- video-player/src/App.jsx
+++ video-player/src/App.jsx
@@ -19,11 +19,13 @@ export default function VideoPlayer() {
     <section className="container">
       <h1>Video Player</h1>
       <article>
-        <video poster="https://image.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/thumbnail.webp">
+        <video
+          ref={videoRef}
+          poster="https://image.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/thumbnail.webp"
+        >
           <source
             src="https://stream.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/high.mp4"
             type="video/mp4"
-            ref={videoRef}
           />
         </video>
         <div>
