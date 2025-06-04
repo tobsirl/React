8f756567a8bebd1c68bff989bdc23f6186b15ede
Paul Tobin <tobster111@gmail.com>
2025-06-04 09:51:54 +0100

    feat: refactor App component to implement video player and remove unused assets

--- video-player/src/App.jsx
+++ video-player/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function VideoPlayer() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="container">
+      <h1>Video Player</h1>
+      <article>
+        <video poster="https://image.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/thumbnail.webp">
+          <source
+            src="https://stream.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/high.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <div>
+          <button
+            title={isPlaying ? "Pause" : "Play"}
+            onClick={handleTogglePlay}
+          >
+            {isPlaying ? "⏸" : "▶"}
+          </button>
+        </div>
+      </article>
+    </section>
+  );
 }
-
-export default App
