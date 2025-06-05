4b256ee064e11840c80087b4201179fecad592f9
Paul Tobin <tobster111@gmail.com>
2025-06-05 11:41:31 +0100

    feat: enhance FieldNotes component with note submission and styling

--- field-notes/src/App.jsx
+++ field-notes/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function FieldNotes() {
+  const [notes, setNotes] = React.useState([
+    "Components encapsulate both the visual representation of a particular piece of UI as well as the state and logic that goes along with it.",
+    "The same intuition you have about creating and composing together functions can directly apply to creating and composing components. However, instead of composing functions together to get some value, you can compose components together to get some UI.",
+    "JSX combines the power and expressiveness of JavaScript with the readability and accessibility of HTML",
+    "Just like a component enabled the composition and reusability of UI, hooks enabled the composition and reusability of non-visual logic.",
+  ]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const newNote = formData.get("note");
+    if (newNote) {
+      setNotes((prevNotes) => [...prevNotes, newNote]);
+      form.reset();
+    }
+  }
 
   return (
-    <>
+    <article>
+      <h1>Field Notes</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {notes.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <input
+            required
+            type="text"
+            name="note"
+            placeholder="Type your note..."
+          />
+          <button className="link" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </article>
+  );
 }
-
-export default App
