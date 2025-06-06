7a312c5f2e12011603fb5c4d677c4747e6ceb82f
Paul Tobin <tobster111@gmail.com>
2025-06-06 12:19:14 +0100

    feat: refactor ClickOutside component and remove unused assets

--- click-outside/src/App.jsx
+++ click-outside/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import { closeIcon } from "./icons";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function ClickOutside() {
+  const isOpen = false;
+  const handleOpenModal = () => {};
+
+  const handleCloseModal = () => {};
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <section>
+        <h1>Click Outside</h1>
+        <button className="link" onClick={handleOpenModal}>
+          Open Modal
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
+      {isOpen && (
+        <dialog>
+          <button onClick={handleCloseModal}>{closeIcon}</button>
+          <h2>Modal</h2>
+          <p>
+            Click outside the modal to close (or use the button) whatever you
+            prefer.
+          </p>
+        </dialog>
+      )}
     </>
-  )
+  );
 }
-
-export default App
