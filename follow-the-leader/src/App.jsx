ce0459c29059851f15ac8ffa8cc481e559120eed
Paul Tobin <tobster111@gmail.com>
2025-06-08 11:00:33 +0100

    refactor: rename positionRef to ref and streamline click handling in FollowTheLeader component

--- follow-the-leader/src/App.jsx
+++ follow-the-leader/src/App.jsx
@@ -3,20 +3,19 @@ import "./App.css";
 
 export default function FollowTheLeader() {
   const [position, setPosition] = React.useState([0, 0]);
-  const positionRef = React.useRef(position);
+  const ref = React.useRef(position);
 
   const handleClick = ({ clientX, clientY }) => {
-    const { width, height } = positionRef.current.getBoundingClientRect();
+    const { width, height } = ref.current.getBoundingClientRect();
 
     setPosition([clientX - width / 2, clientY - height / 2]);
   };
 
   return (
-    <div className="wrapper">
+    <div className="wrapper" onClick={handleClick}>
       <div
         className="box"
-        ref={positionRef}
-        onClick={handleClick}
+        ref={ref}
         style={{
           transform: `translate(${position[0]}px, ${position[1]}px)`,
           transition: "transform 1s",
