fc9d329ff8bd647b88946cb8c2b9e848f4a61ec6
Paul Tobin <tobster111@gmail.com>
2025-06-08 10:58:05 +0100

    feat: implement position tracking and click handling in FollowTheLeader component

--- follow-the-leader/src/App.jsx
+++ follow-the-leader/src/App.jsx
@@ -2,14 +2,21 @@ import * as React from "react";
 import "./App.css";
 
 export default function FollowTheLeader() {
-  const position = [0, 0];
+  const [position, setPosition] = React.useState([0, 0]);
+  const positionRef = React.useRef(position);
 
-  const handleClick = ({ clientX, clientY }) => {};
+  const handleClick = ({ clientX, clientY }) => {
+    const { width, height } = positionRef.current.getBoundingClientRect();
+
+    setPosition([clientX - width / 2, clientY - height / 2]);
+  };
 
   return (
     <div className="wrapper">
       <div
         className="box"
+        ref={positionRef}
+        onClick={handleClick}
         style={{
           transform: `translate(${position[0]}px, ${position[1]}px)`,
           transition: "transform 1s",
