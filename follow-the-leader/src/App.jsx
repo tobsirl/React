dff6ce9c3bdc2a5addf1efffdf3ebdcdf3bdb676
Paul Tobin <tobster111@gmail.com>
2025-06-08 10:49:04 +0100

    feat: refactor FollowTheLeader component and remove unused assets

--- follow-the-leader/src/App.jsx
+++ follow-the-leader/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function FollowTheLeader() {
+  const position = [0, 0];
+
+  const handleClick = ({ clientX, clientY }) => {};
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="wrapper">
+      <div
+        className="box"
+        style={{
+          transform: `translate(${position[0]}px, ${position[1]}px)`,
+          transition: "transform 1s",
+        }}
+      />
+    </div>
+  );
 }
-
-export default App
