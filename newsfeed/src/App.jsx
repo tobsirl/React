2c53b65e95a55dc2f8aea480cf9b87ffce42a95b
Paul Tobin <tobster111@gmail.com>
2025-06-25 19:19:17 +0100

    feat: implement video playback context and provider for managing video state

--- newsfeed/src/App.jsx
+++ newsfeed/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const videoPlaybackContext = React.createContext({
+  playingVideoId: null,
+  setPlayingVideoId: () => {},
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function VideoPlaybackProvider({ children }) {
+  const playingVideoId = null;
+  const setPlayingVideoId = () => {};
+
+  return null;
 }
 
-export default App
+function VideoItem({ videoId, title, poster, src }) {
+  const videoIsActive = false;
+
+  const handleTogglePlay = () => {};
+
+  return (
+    <li>
+      <h3>{title}</h3>
+      <article>
+        <video poster={poster}>
+          <source src={src} type="video/mp4" />
+        </video>
+        <button
+          title={videoIsActive ? "Pause" : "Play"}
+          onClick={handleTogglePlay}
+        >
+          {videoIsActive ? "⏸" : "▶"}
+        </button>
+      </article>
+    </li>
+  );
+}
+
+function NewsFeed() {
+  const videos = [
+    {
+      id: 1,
+      title: "The React Way",
+      poster: "https://react.gg/img/visualized-og2.jpg",
+      src: "https://stream.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/high.mp4",
+    },
+    {
+      id: 2,
+      title: "The History of the Web",
+      poster: "https://react.gg/img/visualized-og1.jpg",
+      src: "https://stream.mux.com/EwJPlEBa0046jGSVdYOnRsX9WnqHjytgIBXwkOt7LvVg/high.mp4",
+    },
+    {
+      id: 3,
+      title: "Rendering, Visualized",
+      poster: "https://react.gg/img/visualized-og5.jpg",
+      src: "https://stream.mux.com/VvQKMwPEOq5BUnc9eRN4sL5sUEZrHqWxNlCbpXSkE3I/high.mp4",
+    },
+  ];
+
+  return (
+    <div>
+      <h1>News Feed</h1>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <VideoPlaybackProvider>
+      <NewsFeed />
+    </VideoPlaybackProvider>
+  );
+}
