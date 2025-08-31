cd8f0f8df3785bf8e79995058e739cf16317335c
Paul Tobin <tobster111@gmail.com>
2025-08-31 12:59:31 +0100

    feat: add @dnd-kit/modifiers dependency and implement vertical axis restriction in RulesList component

--- dnd-sortable-rules/src/components/RulesList.jsx
+++ dnd-sortable-rules/src/components/RulesList.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import RulesItem from "./RulesItem";
 import { DndContext } from "@dnd-kit/core";
 import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
 
 export default function RulesList({ rulesList, setRulesList }) {
   function handleDragEnd(event) {
@@ -10,13 +10,16 @@ export default function RulesList({ rulesList, setRulesList }) {
       setRulesList((items) => {
         const oldIndex = items.findIndex((item) => item.id === active.id);
         const newIndex = items.findIndex((item) => item.id === over.id);
-        return arrayMove(rulesList, oldIndex, newIndex);
+        return arrayMove(items, oldIndex, newIndex);
       });
     }
   }
   return (
     <>
-      <DndContext onDragEnd={handleDragEnd}>
+      <DndContext
+        modifiers={[restrictToVerticalAxis]}
+        onDragEnd={handleDragEnd}
+      >
         <SortableContext items={rulesList}>
           {rulesList.map((rule, idx) => (
             <RulesItem key={rule.id} rule={rule} index={idx + 1} />
