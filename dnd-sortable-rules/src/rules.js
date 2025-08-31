f998e77d2dc926c370c4771dcf1ad1df66a5ecb0
Paul Tobin <tobster111@gmail.com>
2025-08-31 13:13:41 +0100

    feat: update rules with detailed criteria for conversion value testing

--- dnd-sortable-rules/src/rules.js
+++ dnd-sortable-rules/src/rules.js
@@ -1,46 +1,83 @@
 export const rules = [
   {
-    name: "High age",
+    name: "Testing Conversion Value Rules - SM",
     adjust_sign: "INCREASE",
-    adjust_value: 20,
+    adjust_value: 1,
     criterias: [
       {
         criteria_type: "AGE",
         operator: "CONTAINS",
-        criteria_values: ["18-24", "65+"],
-        criteria_value_types: ["NONE", "NONE"],
-        id: "1992718838161786",
-      },
-      {
-        criteria_type: "GENDER",
-        operator: "CONTAINS",
-        criteria_values: ["female"],
+        criteria_values: ["18-24"],
         criteria_value_types: ["NONE"],
-        id: "624391207140536",
-      },
-    ],
-    id: "1093039576293787",
-  },
-  {
-    name: "Toby's Test rule",
-    adjust_sign: "INCREASE",
-    adjust_value: 25,
-    criterias: [
-      {
-        criteria_type: "AGE",
-        operator: "CONTAINS",
-        criteria_values: ["25-34"],
-        criteria_value_types: ["NONE"],
-        id: "2262334934211788",
+        id: "542279348417816",
       },
       {
         criteria_type: "GENDER",
         operator: "CONTAINS",
         criteria_values: ["male"],
         criteria_value_types: ["NONE"],
-        id: "1311484126989224",
+        id: "521501700798533",
       },
     ],
-    id: "765591229186179",
+    id: "1087733916088472",
+  },
+  {
+    name: "Testing Conversion Value Rules - IR01",
+    adjust_sign: "INCREASE",
+    adjust_value: 20,
+    criterias: [
+      {
+        criteria_type: "AGE",
+        operator: "CONTAINS",
+        criteria_values: ["35-44"],
+        criteria_value_types: ["NONE"],
+        id: "577502364933746",
+      },
+      {
+        criteria_type: "GENDER",
+        operator: "CONTAINS",
+        criteria_values: ["female"],
+        criteria_value_types: ["NONE"],
+        id: "1125844302601209",
+      },
+    ],
+    id: "457500186973329",
+  },
+  {
+    name: "Testing Conversion Value Rules - IR02",
+    adjust_sign: "INCREASE",
+    adjust_value: 20,
+    criterias: [
+      {
+        criteria_type: "OS_TYPE",
+        operator: "CONTAINS",
+        criteria_values: ["ios"],
+        criteria_value_types: ["NONE"],
+        id: "951463240239838",
+      },
+      {
+        criteria_type: "AGE",
+        operator: "CONTAINS",
+        criteria_values: ["55-64"],
+        criteria_value_types: ["NONE"],
+        id: "1078512000206753",
+      },
+    ],
+    id: "1110164134024490",
+  },
+  {
+    name: "Testing Conversion Value Rules - IR03",
+    adjust_sign: "INCREASE",
+    adjust_value: 20,
+    criterias: [
+      {
+        criteria_type: "AGE",
+        operator: "CONTAINS",
+        criteria_values: ["45-54"],
+        criteria_value_types: ["NONE"],
+        id: "541971158681528",
+      },
+    ],
+    id: "1371516167146678",
   },
 ];
